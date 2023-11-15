e6ac7e0767042e19a4463676a71571b71dd9cf68
Jin Hyuk <ekrrdj07@gmail.com>
2023-11-15 17:17:40 +0900

    [feat] : 멀티모듈 환경 ComponentScan 설정

--- Ticket-Api/src/main/java/com/jnu/ticketapi/TicketApiApplication.java
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/TicketApiApplication.java
@@ -1,9 +1,12 @@
 package com.jnu.ticketapi;
 
+import com.jnu.ticketdomain.DomainConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(DomainConfig.class)
 public class TicketApiApplication {
 
     public static void main(String[] args) {
