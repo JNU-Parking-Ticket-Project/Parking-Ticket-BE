56d45dfd30ec551fb600302866abd7db1e9a2e7c
Jin Hyuk <ekrrdj07@gmail.com>
2023-11-24 00:14:29 +0900

    [feat] : Filter에서 예외 핸들링

---
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/config/SecurityConfig.java
@@ -0,0 +1,68 @@
+package com.jnu.ticketapi.config;
+
+
+import com.jnu.ticketapi.config.response.JwtAccessDeniedHandler;
+import com.jnu.ticketapi.config.response.JwtAuthenticationEntryPoint;
+import com.jnu.ticketapi.security.JwtAuthenticationFilter;
+import com.jnu.ticketapi.security.JwtExceptionFilter;
+import com.jnu.ticketapi.security.JwtGenerator;
+import com.jnu.ticketapi.security.JwtResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+@Configuration
+public class SecurityConfig {
+    private final JwtGenerator jwtGenerator;
+    private final JwtResolver jwtResolver;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // httpBasic, csrf, formLogin, rememberMe, logout, session disable
+        http.httpBasic()
+                .disable()
+                .csrf()
+                .disable()
+                .formLogin()
+                .disable()
+                .rememberMe()
+                .disable()
+                .logout((logout) -> logout.logoutSuccessUrl("/logout"))
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler);
+
+        // 요청에 대한 권한 설정
+        http.authorizeRequests()
+                .antMatchers("/api/v1/user/login", "/h2-console/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+
+        // jwt filter 설정
+        http.addFilterBefore(
+                new JwtAuthenticationFilter(jwtResolver),
+                UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(new JwtExceptionFilter(), JwtAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
