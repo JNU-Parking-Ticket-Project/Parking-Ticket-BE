d5b5000ea6d327c463db1c3299378c36e09b02bf
Jin Hyuk <ekrrdj07@gmail.com>
2023-11-26 03:33:29 +0900

    [fix] : SecurityConfig 수정
    - 로그인, 정적 리소스, h2, Swagger는 Security Filter에서 제외

--- Ticket-Api/src/main/java/com/jnu/ticketapi/config/SecurityConfig.java
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/config/SecurityConfig.java
@@ -8,10 +8,12 @@ import com.jnu.ticketapi.security.JwtExceptionFilter;
 import com.jnu.ticketapi.security.JwtGenerator;
 import com.jnu.ticketapi.security.JwtResolver;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,7 +21,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @EnableWebSecurity
 @RequiredArgsConstructor
-@Configuration
 public class SecurityConfig {
     private final JwtGenerator jwtGenerator;
     private final JwtResolver jwtResolver;
@@ -33,33 +34,60 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         // httpBasic, csrf, formLogin, rememberMe, logout, session disable
         http.httpBasic()
                 .disable()
-                .csrf().disable()
-                .formLogin().disable()
-                .rememberMe().disable()
-                .logout().disable()
+                .csrf()
+                .disable()
+                .formLogin()
+                .disable()
+                .rememberMe()
+                .disable()
+                .logout()
+                .disable()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .exceptionHandling()
                 .authenticationEntryPoint(jwtAuthenticationEntryPoint)
-                .accessDeniedHandler(jwtAccessDeniedHandler);
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin();
 
         // 요청에 대한 권한 설정
         http.authorizeRequests()
-                .antMatchers("/api/v1/user/login", "/h2-console/**")
+                .antMatchers(
+                        "/swagger-resources/**",
+                        "/swagger-ui/**",
+                        "/swagger-ui.html",
+                        "/v3/api-docs/**",
+                        "/v3/api-docs",
+                        "/api-docs/**",
+                        "/api-docs")
                 .permitAll()
+                .antMatchers("/v1/**")
+                .authenticated()
                 .anyRequest()
-                .authenticated();
+                .denyAll();
 
         // jwt filter 설정
         http.addFilterBefore(
                 new JwtAuthenticationFilter(jwtResolver),
                 UsernamePasswordAuthenticationFilter.class);
         http.addFilterBefore(new JwtExceptionFilter(), JwtAuthenticationFilter.class);
-
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) ->
+                web.ignoring()
+                        .antMatchers(HttpMethod.POST, "/v1/auth/login")
+                        .antMatchers(HttpMethod.GET, "/error")
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+                        .requestMatchers(PathRequest.toH2Console());
     }
 }
