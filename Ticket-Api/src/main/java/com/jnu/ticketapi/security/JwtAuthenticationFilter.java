e2a7193518981834e396a594b5b4fc76fbb06324
Jin Hyuk <ekrrdj07@gmail.com>
2023-11-26 03:15:54 +0900

    [fix] : JwtAuthenticationFilter 수정
    - 로그 추가

--- Ticket-Api/src/main/java/com/jnu/ticketapi/security/JwtAuthenticationFilter.java
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/security/JwtAuthenticationFilter.java
@@ -7,11 +7,13 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @RequiredArgsConstructor
+@Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtResolver jwtResolver;
@@ -20,17 +22,17 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(
             HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+        log.info("JwtAuthenticationFilter 작동중");
         String bearerToken = request.getHeader("Authorization");
         String accessToken = jwtResolver.extractToken(bearerToken);
-
+        log.info("AccessToken : {}", accessToken);
         if (accessToken != null && jwtResolver.accessTokenValidateToken(accessToken)) {
             Authentication authentication = jwtResolver.getAuthentication(accessToken);
-
+            log.info("Authentication : {}", authentication.toString());
             if (authentication != null) {
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             }
         }
-
         filterChain.doFilter(request, response);
     }
 }
