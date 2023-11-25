098b2a3a7f73dde565955887fd74faf81c61e353
Jin Hyuk <ekrrdj07@gmail.com>
2023-11-26 05:22:25 +0900

    [fix] : CustomUserDetailsService 수정
    - CustomUserDetails를 반환하도록 수정

--- Ticket-Api/src/main/java/com/jnu/ticketapi/application/service/CustomUserDetailsService.java
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/application/service/CustomUserDetailsService.java
@@ -1,11 +1,10 @@
 package com.jnu.ticketapi.application.service;
 
 
+import com.jnu.ticketapi.security.CustomUserDetails;
 import com.jnu.ticketdomain.domain.user.User;
 import com.jnu.ticketdomain.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
@@ -17,15 +16,11 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public CustomUserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         final User user =
                 userRepository
-                        .findByEmail(username)
+                        .findByEmail(email)
                         .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다."));
-
-        return new org.springframework.security.core.userdetails.User(
-                user.getEmail(),
-                user.getPwd(),
-                AuthorityUtils.createAuthorityList(user.getUserRole().getValue()));
+        return new CustomUserDetails(user);
     }
 }
