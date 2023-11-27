940cba4d072384c02ac2c1ce484837982384bdc1
이서현 <ymecca12@gmail.com>
2023-11-27 19:49:01 +0900

    [feat] : ticket domain conflict resolve

--- Ticket-Domain/src/main/java/com/jnu/ticketdomain/TicketDomainApplication.java
+++ Ticket-Domain/src/main/java/com/jnu/ticketdomain/TicketDomainApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TicketDomainApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(TicketDomainApplication.class, args);
     }
