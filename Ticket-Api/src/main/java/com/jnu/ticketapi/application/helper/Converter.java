c6b64a9dc2d4167b07f1e6b322243686982fd2bf
Jin Hyuk <ekrrdj07@gmail.com>
2023-11-28 12:11:22 +0900

    [feat] : SectorToDto Converter
    - List<Sector>를 받아서 List<SectorDto>를 반환하는 Converter

---
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/application/helper/Converter.java
@@ -0,0 +1,20 @@
+package com.jnu.ticketapi.application.helper;
+
+import com.jnu.ticketapi.dto.SectorDto;
+import com.jnu.ticketcommon.annotation.Helper;
+import com.jnu.ticketdomain.domains.coupon.domain.Sector;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Helper
+public class Converter {
+    public  List<SectorDto> sectorToDto(List<Sector> sectorList) {
+        return sectorList.stream()
+                .map((sector) -> SectorDto.builder()
+                        .sectorId(sector.getId())
+                        .sectorName(sector.getSectorNumber())
+                        .sectionColleges(sector.getName())
+                        .build())
+                .collect(Collectors.toList());
+    }
+}
