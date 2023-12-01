0ea2533228c1feaf4d9e5800e848812d4064910e
blackbean <ymecca730135@gmail.com>
2023-12-01 12:52:44 +0900

    [feat] : CI_Check subproject java binaries property 수정

--- Ticket-Infrastructure/src/test/java/com/jnu/ticketinfrastructure/InfraIntegrateSpringBootTest.java
+++ Ticket-Infrastructure/src/test/java/com/jnu/ticketinfrastructure/InfraIntegrateSpringBootTest.java
@@ -1,4 +1,4 @@
-package com.jnu.ticketapi.user.supports;
+package com.jnu.ticketinfrastructure;
 
 
 import java.lang.annotation.Documented;
@@ -7,12 +7,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 /** 도메인 모듈의 통합테스트의 편의성을 위해서 만든 어노테이션 */
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-@SpringBootTest(classes = ApiIntegrateTestConfig.class)
-@ActiveProfiles(resolver = ApiIntegrateProfileResolver.class)
+@SpringBootTest(classes = InfraIntegrateTestConfig.class)
 @Documented
-public @interface ApiIntegrateSpringBootTest {}
+public @interface InfraIntegrateSpringBootTest {}
