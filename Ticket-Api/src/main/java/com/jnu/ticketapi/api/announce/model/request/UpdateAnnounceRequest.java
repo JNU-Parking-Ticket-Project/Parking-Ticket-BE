fc9173bbc9eb1ff5d377de5a71038a842cfb16a7
Cookie <pon05114@naver.com>
2023-12-04 20:12:25 +0900

    [feat] : 공지사항 수정 Request DTO

---
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/api/announce/model/request/UpdateAnnounceRequest.java
@@ -0,0 +1,11 @@
+package com.jnu.ticketapi.api.announce.model.request;
+
+import lombok.Builder;
+
+public record UpdateAnnounceRequest(
+        String announceTitle,
+        String announceContent
+) {
+    @Builder
+    public UpdateAnnounceRequest{}
+}
