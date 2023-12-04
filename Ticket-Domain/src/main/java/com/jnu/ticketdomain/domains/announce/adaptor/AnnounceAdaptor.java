f7f17a699e5a9cc23050bfff53794bcbe0b5fc9a
Cookie <pon05114@naver.com>
2023-12-04 10:12:33 +0900

    [feat] : 공지사항 조회 Adaptor

---
+++ Ticket-Domain/src/main/java/com/jnu/ticketdomain/domains/announce/adaptor/AnnounceAdaptor.java
@@ -0,0 +1,27 @@
+package com.jnu.ticketdomain.domains.announce.adaptor;
+
+import com.jnu.ticketcommon.annotation.Adaptor;
+import com.jnu.ticketdomain.domains.announce.domain.Announce;
+import com.jnu.ticketdomain.domains.announce.out.AnnounceLoadPort;
+import com.jnu.ticketdomain.domains.announce.out.AnnounceRecordPort;
+import com.jnu.ticketdomain.domains.announce.repository.AnnounceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+@Adaptor
+@RequiredArgsConstructor
+public class AnnounceAdaptor implements AnnounceRecordPort, AnnounceLoadPort {
+
+    private final AnnounceRepository announceRepository;
+
+    @Override
+    public Announce save(Announce announce) {
+        return null;
+    }
+
+    @Override
+    public Page<Announce> findAllByOrderByCreatedAtDesc(Pageable pageable) {
+        return announceRepository.findAllByOrderByCreatedAtDesc(pageable);
+    }
+}
