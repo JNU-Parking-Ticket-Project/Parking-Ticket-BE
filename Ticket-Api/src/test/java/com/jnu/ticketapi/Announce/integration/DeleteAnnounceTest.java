588af1d8b60db142626feeda715faff3cf6c0321
Cookie <pon05114@naver.com>
2023-12-05 04:38:06 +0900

    [feat] : 공지사항 삭제 통합 테스트

---
+++ Ticket-Api/src/test/java/com/jnu/ticketapi/Announce/integration/DeleteAnnounceTest.java
@@ -0,0 +1,99 @@
+package com.jnu.ticketapi.Announce.integration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jnu.ticketapi.api.announce.model.request.SaveAnnounceRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import java.nio.charset.StandardCharsets;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Slf4j
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+public class DeleteAnnounceTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper om;
+
+    @Test
+    @DisplayName("성공 : 공지사항 삭제")
+    @WithMockUser
+    void save_announces_test() throws Exception {
+        {
+            // given
+            SaveAnnounceRequest saveRequest = SaveAnnounceRequest.builder()
+                    .announceTitle("테스트 제목")
+                    .announceContent("테스트 내용")
+                    .build();
+            String testRequestBody = om.writeValueAsString(saveRequest);
+
+            ResultActions testResultActions =
+                    mvc.perform(
+                            post("/v1/announce")
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .characterEncoding(StandardCharsets.UTF_8)
+                                    .content(testRequestBody));
+            String testResponseBody = testResultActions.andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+            log.info("responseBody : " + testResponseBody);
+
+            long announceId = 1L;
+
+
+
+            // when
+            ResultActions resultActions =
+                    mvc.perform(
+                            delete("/v1/announce/" + announceId)
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .characterEncoding(StandardCharsets.UTF_8)
+                    );
+
+            // eye
+            String responseBody = resultActions.andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+            log.info("responseBody : " + responseBody);
+            // then
+            resultActions.andExpectAll(
+                    status().isOk(),
+                    jsonPath("$.success").value(true));
+        }
+    }
+
+    @Test
+    @DisplayName("실패 : 공지사항 목록 삭제(존재하지 않는 공지사항 ID 입력)")
+    @WithMockUser
+    void delete_announces_test() throws Exception {
+        {
+            // given
+            long announceId = 55L;
+
+
+            // when
+            ResultActions resultActions =
+                    mvc.perform(
+                            delete("/v1/announce/"+announceId)
+                                    .contentType(MediaType.APPLICATION_JSON));
+            // eye
+            String responseBody = resultActions.andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+            log.info("responseBody : " + responseBody);
+            // then
+            resultActions.andExpectAll(
+                    status().is4xxClientError(),
+                    jsonPath("$.success").value(false));
+
+        }
+    }
+}
