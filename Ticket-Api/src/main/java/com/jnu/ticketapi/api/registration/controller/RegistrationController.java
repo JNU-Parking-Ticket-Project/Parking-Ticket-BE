6684b48d653dfc131f4a84b0cd7c508f5ab8305e
Jin Hyuk <ekrrdj07@gmail.com>
2023-12-04 19:29:57 +0900

    [fix] : Controller 수정
    - Swagger에 적용할 Tag, Operation 작성

--- Ticket-Api/src/main/java/com/jnu/ticketapi/api/registration/controller/RegistrationController.java
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/api/registration/controller/RegistrationController.java
@@ -9,7 +9,9 @@ import com.jnu.ticketapi.api.registration.model.response.TemporarySaveResponse;
 import com.jnu.ticketapi.api.user.service.UserUseCase;
 import com.jnu.ticketapi.application.port.RegistrationUseCase;
 import com.jnu.ticketapi.common.aop.GetEmail;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,16 +20,21 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/v1")
 @RequiredArgsConstructor
+@Tag(name = "4. [신청]")
 public class RegistrationController {
     private final RegistrationUseCase registrationUseCase;
     private final UserUseCase userUseCase;
 
+    @Operation(
+            summary = "임시 저장 조회",
+            description = "임시 저장 했던 정보를 조회(임시 저장을 하지 않은 유저는 Email, Sector 빼고 null 반환)")
     @GetMapping("/registration")
     public ResponseEntity<GetRegistrationResponse> getRegistration(@GetEmail String email) {
         GetRegistrationResponse responseDto = registrationUseCase.getRegistration(email);
         return ResponseEntity.ok(responseDto);
     }
 
+    @Operation(summary = "임시 저장", description = "임시 저장")
     @PostMapping("/registration/false")
     public ResponseEntity<TemporarySaveResponse> temporarySave(
             @RequestBody TemporarySaveRequest requestDto) {
@@ -35,6 +42,7 @@ public class RegistrationController {
         return ResponseEntity.ok(responseDto);
     }
 
+    @Operation(summary = "1차 신청", description = "1차 신청")
     @PostMapping("/registration/true")
     public ResponseEntity<FinalSaveResponse> finalSave(@RequestBody FinalSaveRequest requestDto) {
         FinalSaveResponse responseDto = registrationUseCase.finalSave(requestDto);
