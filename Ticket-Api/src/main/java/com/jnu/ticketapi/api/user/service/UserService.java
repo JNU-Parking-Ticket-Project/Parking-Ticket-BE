47196699a6a02a30b48f806b98f391703cc7568f
Jin Hyuk <ekrrdj07@gmail.com>
2023-12-04 17:56:53 +0900

    [feat] : 권한 설정 비지니스

--- Ticket-Api/src/main/java/com/jnu/ticketapi/api/user/service/UserService.java
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/api/user/service/UserService.java
@@ -1,9 +1,14 @@
 package com.jnu.ticketapi.api.user.service;
 
 
+import com.jnu.ticketapi.api.user.model.response.UpdateRoleResponse;
+import com.jnu.ticketapi.application.helper.Converter;
 import com.jnu.ticketdomain.domains.user.adaptor.UserAdaptor;
 import com.jnu.ticketdomain.domains.user.domain.User;
 import java.util.Optional;
+
+import com.jnu.ticketdomain.domains.user.domain.UserRole;
+import com.jnu.ticketdomain.domains.user.exception.NotFoundUserException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -14,6 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 public class UserService implements UserUseCase {
     private final UserAdaptor userAdaptor;
+    private final Converter converter;
 
     @Transactional(readOnly = true)
     @Override
@@ -21,9 +27,24 @@ public class UserService implements UserUseCase {
         return userAdaptor.findByEmail(email);
     }
 
+    @Transactional(readOnly = true)
+    @Override
+    public Optional<User> findById(Long userId) { return userAdaptor.findById(userId); }
+
     @Override
     @Transactional
     public User save(User user) {
         return userAdaptor.save(user);
+    }
+
+    @Override
+    @Transactional
+    public UpdateRoleResponse updateRole(Long userId, String role) {
+        User user = findById(userId)
+                .orElseThrow(
+                        () -> NotFoundUserException.EXCEPTION
+        );
+        user.updateRole(UserRole.valueOf(role));
+        return converter.toUpdateRoleResponseDto(user);
     }
 }
