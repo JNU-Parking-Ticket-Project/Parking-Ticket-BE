62eda020e52116aad601b5bc8fdaca004531d2af
Cookie <pon05114@naver.com>
2023-12-04 10:12:21 +0900

    [feat] : 공지사항 조회 UseCase

---
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/api/announce/service/GetAnnouncesUseCase.java
@@ -0,0 +1,29 @@
+package com.jnu.ticketapi.api.announce.service;
+
+import com.jnu.ticketapi.api.announce.model.response.AnnouncePagingResponse;
+import com.jnu.ticketcommon.annotation.UseCase;
+import com.jnu.ticketdomain.domains.announce.adaptor.AnnounceAdaptor;
+import com.jnu.ticketdomain.domains.announce.domain.Announce;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
+
+@UseCase
+@RequiredArgsConstructor
+@Slf4j
+public class GetAnnouncesUseCase {
+
+    private final AnnounceAdaptor announceAdaptor;
+
+    @Transactional(readOnly = true)
+    public AnnouncePagingResponse execute(Pageable pageable) {
+        Page<Announce> announcePage = announceAdaptor.findAllByOrderByCreatedAtDesc(pageable);
+        return AnnouncePagingResponse.of(announcePage);
+    }
+
+
+
+
+}
