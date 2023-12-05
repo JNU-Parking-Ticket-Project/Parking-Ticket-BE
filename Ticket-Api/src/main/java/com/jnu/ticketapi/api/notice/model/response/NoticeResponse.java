40e19e73b5e65e7c2fb53c3b03dd5679cd32230f
Cookie <pon05114@naver.com>
2023-12-05 15:35:27 +0900

    [feat] : 안내사항 조회 Response DTO

---
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/api/notice/model/response/NoticeResponse.java
@@ -0,0 +1,17 @@
+package com.jnu.ticketapi.api.notice.model.response;
+
+import com.jnu.ticketdomain.domains.notice.domain.Notice;
+import lombok.Builder;
+
+public record NoticeResponse(
+        String noticeContent
+) {
+    @Builder
+    public NoticeResponse{}
+
+    public static NoticeResponse of(Notice notice){
+        return NoticeResponse.builder()
+                .noticeContent(notice.getNoticeContent())
+                .build();
+    }
+}
