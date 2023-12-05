5fdb8231bc01e1f4eedf128707822e6ee7ffb7e0
Cookie <pon05114@naver.com>
2023-12-05 20:18:24 +0900

    [docs] : 공지사항 swagger 예외 추가

---
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/api/announce/docs/UpdateAnnounceExceptionDocs.java
@@ -0,0 +1,14 @@
+package com.jnu.ticketapi.api.announce.docs;
+
+import com.jnu.ticketcommon.annotation.ExceptionDoc;
+import com.jnu.ticketcommon.annotation.ExplainError;
+import com.jnu.ticketcommon.exception.AnnounceNotExistException;
+import com.jnu.ticketcommon.exception.TicketCodeException;
+import com.jnu.ticketcommon.interfaces.SwaggerExampleExceptions;
+
+@ExceptionDoc
+public class UpdateAnnounceExceptionDocs implements SwaggerExampleExceptions {
+    @ExplainError("업데이트 할 공지사항이 존재하지 않는 경우")
+    public TicketCodeException 업데이트할_공지사항이_존재하지_않음 = AnnounceNotExistException.EXCEPTION;
+
+}
