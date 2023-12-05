6f4b8a65a7036b56e50b9f1b0dcfaba4707aa9f9
Cookie <pon05114@naver.com>
2023-12-05 15:36:22 +0900

    [feat] : 안내사항 수정 UseCase

---
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/api/notice/service/UpdateNoticeUseCase.java
@@ -0,0 +1,22 @@
+package com.jnu.ticketapi.api.notice.service;
+
+import com.jnu.ticketapi.api.notice.model.request.UpdateNoticeRequest;
+import com.jnu.ticketapi.api.notice.model.response.UpdateNoticeResponse;
+import com.jnu.ticketcommon.annotation.UseCase;
+import com.jnu.ticketdomain.domains.notice.adaptor.NoticeAdaptor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
+
+@UseCase
+@RequiredArgsConstructor
+@Slf4j
+public class UpdateNoticeUseCase {
+
+    private final NoticeAdaptor noticeAdaptor;
+
+    @Transactional
+    public UpdateNoticeResponse updateNotice(UpdateNoticeRequest updateNoticeRequest){
+        return UpdateNoticeResponse.of(noticeAdaptor.updateNotice(updateNoticeRequest.noticeContent()));
+    }
+}
