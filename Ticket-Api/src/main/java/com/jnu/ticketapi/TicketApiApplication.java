273dbe91a640bbb4066d089eb008a397b2872d67
Jin Hyuk <ekrrdj07@gmail.com>
2023-12-05 12:53:43 +0900

    [fix] : TicketApiApplication
    - CORS 설정 추가

--- Ticket-Api/src/main/java/com/jnu/ticketapi/TicketApiApplication.java
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/TicketApiApplication.java
@@ -8,8 +8,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @RequiredArgsConstructor
@@ -25,5 +28,18 @@ public class TicketApiApplication implements ApplicationListener<ApplicationRead
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
         log.info("applicationReady status" + Arrays.toString(environment.getActiveProfiles()));
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() { // 전역적으로 cors에러 해결법 Spring mvc에서 사용
+        return new WebMvcConfigurer() {
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
