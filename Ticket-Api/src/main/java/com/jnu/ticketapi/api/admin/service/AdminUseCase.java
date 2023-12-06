b091b6ce3b5fad4d1e2ce0a5e7612359a2593ffa
Jin Hyuk <ekrrdj07@gmail.com>
2023-12-06 17:54:52 +0900

    [feat] : 권한 설정 비지니스

---
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/api/admin/service/AdminUseCase.java
@@ -0,0 +1,21 @@
+package com.jnu.ticketapi.api.admin.service;
+
+
+import com.jnu.ticketapi.api.user.model.response.UpdateRoleResponse;
+import com.jnu.ticketcommon.annotation.UseCase;
+import com.jnu.ticketdomain.domains.admin.adaptor.AdminAdaptor;
+import com.jnu.ticketdomain.domains.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+
+@UseCase
+@RequiredArgsConstructor
+public class AdminUseCase {
+    private final AdminAdaptor adminAdaptor;
+
+    @Transactional
+    public UpdateRoleResponse updateRole(Long userId, String role) {
+        User user = adminAdaptor.updateRole(userId, role);
+        return UpdateRoleResponse.of(user);
+    }
+}
