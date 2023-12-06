0ecfd20b2de2f5bb51dc61516124e7cf8c86fc21
Cookie <pon05114@naver.com>
2023-12-06 20:04:04 +0900

    [feat] : 비밀번호 재설정 관련 인증 코드 Repository 미사용 Method 삭제

--- Ticket-Domain/src/main/java/com/jnu/ticketdomain/domains/CredentialCode/repository/CredentialCodeRepository.java
+++ Ticket-Domain/src/main/java/com/jnu/ticketdomain/domains/CredentialCode/repository/CredentialCodeRepository.java
@@ -4,11 +4,12 @@ import com.jnu.ticketdomain.domains.CredentialCode.domain.CredentialCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CredentialCodeRepository extends JpaRepository<CredentialCode, Long> {
 
-    CredentialCode findByEmail(String email);
-    CredentialCode findByCode(String code);
+    Optional<CredentialCode> findByCode(String code);
     boolean existsByEmail(String email);
     void deleteByEmail(String email);
 }
