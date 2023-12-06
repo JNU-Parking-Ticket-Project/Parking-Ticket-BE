820a79009fa27eb99eb318ced6df83b0cfe3899b
Cookie <pon05114@naver.com>
2023-12-06 19:04:39 +0900

    [docs] : 비밀번호 찾기 메일 전송 JavaDoc

--- Ticket-Api/src/main/java/com/jnu/ticketapi/api/user/service/CredentialCodeUseCase.java
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/api/user/service/CredentialCodeUseCase.java
@@ -14,6 +14,13 @@ import org.thymeleaf.context.Context;
 
 import java.util.UUID;
 
+/**
+ * 비밀번호 재설정을 하기 위한 링크를 생성하고, 메일 전송한다.
+ * 비밀번호 재설정을 하기 위한 링크는 UUID 클래스를 통해 랜덤 생성한다.
+ *
+ * @author cookie
+ * @version 1.0
+ */
 @RequiredArgsConstructor
 @UseCase
 @Slf4j
@@ -22,19 +29,27 @@ public class CredentialCodeUseCase {
     private final CredentialCodeAdaptor credentialCodeAdaptor;
     private final MailService mailService;
 
+    /**
+     * 비밀번호 재설정 링크 생성하고 이를 포함한 메일을 사용자가 입력한 이메일 주소로 전송한다.
+     *
+     * @param findPasswordRequest : 비밀번호 재설정을 위한 이메일 문자열이 들어있다.
+     * @return FindPasswordResponse : boolean 값을 통해 메일이 전송되었는지 유무를 문자열로 반환한다.
+     */
     @Transactional
     public FindPasswordResponse sendMail(FindPasswordRequest findPasswordRequest){
-        String code = UUID.randomUUID().toString();
-        CredentialCode credentialCode = credentialCodeAdaptor.saveCode(findPasswordRequest.toEntity(code));
+        CredentialCode credentialCode = credentialCodeAdaptor.saveCode(findPasswordRequest
+                .toEntity(UUID.randomUUID().toString()));
+
         Context context = new Context();
-        context.setVariable(MailTemplate.FIND_PASSWORD_CONTEXT, credentialCode.getCode());
-        boolean result;
+        context.setVariable(MailTemplate.FIND_PASSWORD_CONTEXT, MailTemplate.URL + credentialCode.getCode());
+
         try{
-            result = mailService.sendMail(credentialCode.getEmail(), MailTemplate.FIND_PASSWORD_SUBJECT, MailTemplate.FIND_PASSWORD_TEMPLATE, context);
+            boolean result = mailService.sendMail(credentialCode.getEmail(), MailTemplate.FIND_PASSWORD_SUBJECT, MailTemplate.FIND_PASSWORD_TEMPLATE, context);
+            return FindPasswordResponse.of(result);
         }catch (Exception e){
             log.info("발송 실패 : {}", e.getMessage());
             return FindPasswordResponse.of(false);
         }
-        return FindPasswordResponse.of(result);
+
     }
 }
