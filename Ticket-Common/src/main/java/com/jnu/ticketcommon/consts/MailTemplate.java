18c81f433a954079e1758f767f686cf18edd32e6
Cookie <pon05114@naver.com>
2023-12-06 19:05:28 +0900

    [feat] : 비밀번호 찾기 메일 전송 상수 문자열 URL 추가

--- Ticket-Common/src/main/java/com/jnu/ticketcommon/consts/MailTemplate.java
+++ Ticket-Common/src/main/java/com/jnu/ticketcommon/consts/MailTemplate.java
@@ -5,4 +5,6 @@ public class MailTemplate {
     public static final String FIND_PASSWORD_TEMPLATE = "findPassword";
     public static final String FIND_PASSWORD_SUBJECT = "주차권 신청 서비스 비밀번호 재설정 메일";
     public static final String FIND_PASSWORD_CONTEXT = "resetLink";
+
+    public static final String URL = "http://3.35.13.95:8080/change/password/";
 }
