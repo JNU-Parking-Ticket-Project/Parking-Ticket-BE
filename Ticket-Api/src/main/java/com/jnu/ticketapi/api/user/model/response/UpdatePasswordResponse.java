243c182586136c3b8be3c0ba116de16914098ad5
Cookie <pon05114@naver.com>
2023-12-06 20:05:03 +0900

    [feat] : 비밀번호 재설정 Response DTO

---
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/api/user/model/response/UpdatePasswordResponse.java
@@ -0,0 +1,17 @@
+package com.jnu.ticketapi.api.user.model.response;
+
+import com.jnu.ticketdomain.domains.user.domain.User;
+import lombok.Builder;
+
+public record UpdatePasswordResponse(
+        String email
+) {
+    @Builder
+    public UpdatePasswordResponse{}
+
+    public static UpdatePasswordResponse of(User user){
+        return UpdatePasswordResponse.builder()
+                .email(user.getEmail())
+                .build();
+    }
+}
