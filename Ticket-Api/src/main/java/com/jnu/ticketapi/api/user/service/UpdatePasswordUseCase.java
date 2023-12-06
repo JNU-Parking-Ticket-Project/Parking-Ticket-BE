f9bb4e796f8d8bf44f79e3fec2abeac3072d346e
Cookie <pon05114@naver.com>
2023-12-06 20:05:13 +0900

    [feat] : 비밀번호 재설정 UseCase

---
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/api/user/service/UpdatePasswordUseCase.java
@@ -0,0 +1,29 @@
+package com.jnu.ticketapi.api.user.service;
+
+import com.jnu.ticketapi.api.user.model.request.UpdatePasswordRequest;
+import com.jnu.ticketapi.api.user.model.response.UpdatePasswordResponse;
+import com.jnu.ticketcommon.annotation.UseCase;
+import com.jnu.ticketdomain.domains.CredentialCode.adaptor.CredentialCodeAdaptor;
+import com.jnu.ticketdomain.domains.user.adaptor.UserAdaptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
+@UseCase
+@RequiredArgsConstructor
+public class UpdatePasswordUseCase {
+
+    private final UserAdaptor userAdaptor;
+    private final CredentialCodeAdaptor credentialCodeAdaptor;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Transactional
+    public UpdatePasswordResponse execute(String code, UpdatePasswordRequest updatePasswordRequest){
+
+        return UpdatePasswordResponse.of(
+                userAdaptor.updatePassword(credentialCodeAdaptor.getEmail(code),
+                        bCryptPasswordEncoder.encode(updatePasswordRequest.password()))
+        );
+
+    }
+}
