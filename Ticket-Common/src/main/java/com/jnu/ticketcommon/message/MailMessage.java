2620809ceabc1dae29e2a013dadcceb510f4d098
Cookie <pon05114@naver.com>
2023-12-06 18:36:23 +0900

    [test] : 비밀번호 찾기 메일 전송메세지

---
+++ Ticket-Common/src/main/java/com/jnu/ticketcommon/message/MailMessage.java
@@ -0,0 +1,7 @@
+package com.jnu.ticketcommon.message;
+
+public class MailMessage {
+
+    public static final String SUCCESS_SEND_MAIL = "메일 발송 성공";
+    public static final String FAIL_SEND_MAIL = "메일 발송 실패";
+}
