3de1489cfb20027eb812461a56688dfe7f5c222b
Cookie <pon05114@naver.com>
2023-12-06 18:33:37 +0900

    [feat] : 비밀번호 찾기 메일 전송 Request DTO

---
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/api/user/model/request/FindPasswordRequest.java
@@ -0,0 +1,18 @@
+package com.jnu.ticketapi.api.user.model.request;
+
+import com.jnu.ticketdomain.domains.CredentialCode.domain.CredentialCode;
+import lombok.Builder;
+
+public record FindPasswordRequest(
+        String email
+) {
+    @Builder
+    public FindPasswordRequest{}
+
+    public CredentialCode toEntity(String code){
+        return CredentialCode.builder()
+                .code(code)
+                .email(this.email)
+                .build();
+    }
+}
