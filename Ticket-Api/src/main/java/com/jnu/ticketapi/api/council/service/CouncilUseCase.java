9ae1e47bd142de9768137f4e29b2e3ebf6c74548
Jin Hyuk <ekrrdj07@gmail.com>
2023-12-07 12:38:30 +0900

    [fix] : CouncilUseCase
    - User에 관한건 UserAdaptor 사용

--- Ticket-Api/src/main/java/com/jnu/ticketapi/api/council/service/CouncilUseCase.java
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/api/council/service/CouncilUseCase.java
@@ -7,7 +7,9 @@ import com.jnu.ticketcommon.annotation.UseCase;
 import com.jnu.ticketcommon.message.ResponseMessage;
 import com.jnu.ticketdomain.domains.council.adaptor.CouncilAdaptor;
 import com.jnu.ticketdomain.domains.council.domain.Council;
+import com.jnu.ticketdomain.domains.user.adaptor.UserAdaptor;
 import com.jnu.ticketdomain.domains.user.domain.User;
+import com.jnu.ticketdomain.domains.user.exception.NotFoundUserException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,18 +17,19 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class CouncilUseCase {
     private final CouncilAdaptor councilAdaptor;
+    private final UserAdaptor userAdaptor;
 
     @Transactional(readOnly = true)
     public User findByEmail(String email) {
-        return councilAdaptor.findByEmail(email);
+        return userAdaptor.findByEmail(email).orElseThrow(() -> NotFoundUserException.EXCEPTION);
     }
 
     @Transactional
     public SignUpCouncilResponse signUp(SignUpCouncilRequest signUpCouncilRequest) {
         User user = signUpCouncilRequest.toUserEntity(signUpCouncilRequest);
         Council council = signUpCouncilRequest.toCouncilEntity(signUpCouncilRequest, user);
-        councilAdaptor.saveUser(user);
-        councilAdaptor.saveCouncil(council);
+        userAdaptor.save(user);
+        councilAdaptor.save(council);
         return SignUpCouncilResponse.of(ResponseMessage.SUCCESS_SIGN_UP);
     }
 }
