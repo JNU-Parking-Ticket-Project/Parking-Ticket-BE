6d44aa720b894bdbe35fab13f2f5067486bc375f
Jin Hyuk <ekrrdj07@gmail.com>
2023-12-07 12:37:17 +0900

    [fix] : CouncilRecordPort
    - CouncilRecordPort에서 User 도메인 분리

--- Ticket-Domain/src/main/java/com/jnu/ticketdomain/domains/council/out/CouncilRecordPort.java
+++ Ticket-Domain/src/main/java/com/jnu/ticketdomain/domains/council/out/CouncilRecordPort.java
@@ -3,11 +3,8 @@ package com.jnu.ticketdomain.domains.council.out;
 
 import com.jnu.ticketcommon.annotation.Port;
 import com.jnu.ticketdomain.domains.council.domain.Council;
-import com.jnu.ticketdomain.domains.user.domain.User;
 
 @Port
 public interface CouncilRecordPort {
-    User saveUser(User user);
-
-    Council saveCouncil(Council council);
+    Council save(Council council);
 }
