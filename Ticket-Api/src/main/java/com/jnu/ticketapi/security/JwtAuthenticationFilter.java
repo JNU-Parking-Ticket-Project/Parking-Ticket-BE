c0db16327cb6d16e312c8aeff3d73039c4a7c96b
Jin Hyuk <ekrrdj07@gmail.com>
2023-12-10 12:10:17 +0900

    [hotfix] : JwtAuthenticationFilter
    - try, catch문 삭제

--- Ticket-Api/src/main/java/com/jnu/ticketapi/security/JwtAuthenticationFilter.java
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/security/JwtAuthenticationFilter.java
@@ -1,7 +1,6 @@
 package com.jnu.ticketapi.security;
 
 
-import com.jnu.ticketcommon.exception.InvalidTokenException;
 import java.io.IOException;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -24,21 +23,16 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         log.info("JwtAuthenticationFilter 작동중");
-        try {
-            String bearerToken = request.getHeader("Authorization");
-            String accessToken = jwtResolver.extractToken(bearerToken);
-            log.info("AccessToken : {}", accessToken);
-            if (accessToken != null && jwtResolver.accessTokenValidateToken(accessToken)) {
-                Authentication authentication = jwtResolver.getAuthentication(accessToken);
-                log.info("Authentication : {}", authentication.toString());
-                if (authentication != null) {
-                    SecurityContextHolder.getContext().setAuthentication(authentication);
-                }
+        String bearerToken = request.getHeader("Authorization");
+        String accessToken = jwtResolver.extractToken(bearerToken);
+        log.info("AccessToken : {}", accessToken);
+        if (accessToken != null && jwtResolver.accessTokenValidateToken(accessToken)) {
+            Authentication authentication = jwtResolver.getAuthentication(accessToken);
+            log.info("Authentication : {}", authentication.toString());
+            if (authentication != null) {
+                SecurityContextHolder.getContext().setAuthentication(authentication);
             }
-            filterChain.doFilter(request, response);
-        } catch (Exception e) {
-            log.error("JwtAuthenticationFilter 오류 : {}", e.getMessage());
-            throw InvalidTokenException.EXCEPTION;
         }
+        filterChain.doFilter(request, response);
     }
 }
