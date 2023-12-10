1738c96910d5553abfeed3c5acebc579a26f06e9
Cookie <pon05114@naver.com>
2023-12-10 15:06:13 +0900

    [refactor] : spotless 적용

--- Ticket-Api/src/main/java/com/jnu/ticketapi/api/user/docs/UpdatePasswordExceptionDocs.java
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/api/user/docs/UpdatePasswordExceptionDocs.java
@@ -1,5 +1,6 @@
 package com.jnu.ticketapi.api.user.docs;
 
+
 import com.jnu.ticketcommon.annotation.ExplainError;
 import com.jnu.ticketcommon.exception.TicketCodeException;
 import com.jnu.ticketcommon.interfaces.SwaggerExampleExceptions;
@@ -10,6 +11,7 @@ public class UpdatePasswordExceptionDocs implements SwaggerExampleExceptions {
 
     @ExplainError("비밀번호 변경할 사용자가 존재하지 않는 경우")
     public TicketCodeException 사용자가_존재하지_않음 = NotFoundUserException.EXCEPTION;
+
     @ExplainError("발급된 인증링크가 잘못된 경우")
     public TicketCodeException 잘못된_링크_접근 = CredentialCodeNotExistException.EXCEPTION;
 }
