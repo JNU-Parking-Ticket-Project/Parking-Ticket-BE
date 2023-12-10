980d1fdcbff1f3518e7b02cb861a984a692e8eab
blackbean <ymecca730135@gmail.com>
2023-12-10 17:28:55 +0900

    [feat] : Phone Annotation

---
+++ Ticket-Common/src/main/java/com/jnu/ticketcommon/annotation/Phone.java
@@ -0,0 +1,20 @@
+package com.jnu.ticketcommon.annotation;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import javax.validation.Constraint;
+
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = PhoneValidator.class)
+public @interface Phone {
+
+  String message() default "";
+
+  Class[] groups() default {};
+
+  Class[] payload() default {};
+}
