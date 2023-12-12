52a8b72047a409696dc16ca582f058c61c9d2a1b
Jin Hyuk <ekrrdj07@gmail.com>
2023-12-12 23:44:05 +0900

    [test] : 권한 수정 통합 테스트

---
+++ Ticket-Common/src/main/java/com/jnu/ticketcommon/message/ValidationMessage.java
@@ -0,0 +1,7 @@
+package com.jnu.ticketcommon.message;
+
+public class ValidationMessage {
+    public static final String INVALID_ROLE_VALUE = "ROLE을 정확히 입력 해주세요.";
+
+    private ValidationMessage() {}
+}
