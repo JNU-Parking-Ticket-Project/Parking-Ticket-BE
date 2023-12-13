6bb85393b8128ab76917441e2ccfb5a7e05be163
Jin Hyuk <ekrrdj07@gmail.com>
2023-12-13 21:54:34 +0900

    [test] : 이메일 중복 체크 통합 테스트

---
+++ Ticket-Api/src/test/java/com/jnu/ticketapi/auth/CheckEmailTest.java
@@ -0,0 +1,115 @@
+package com.jnu.ticketapi.auth;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jnu.ticketapi.api.auth.model.request.CheckEmailRequest;
+import com.jnu.ticketapi.api.auth.model.request.LoginUserRequest;
+import com.jnu.ticketcommon.message.ResponseMessage;
+import com.jnu.ticketcommon.message.ValidationMessage;
+import com.jnu.ticketdomain.domains.council.exception.CouncilErrorCode;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Slf4j
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@Sql("classpath:db/teardown.sql")
+public class CheckEmailTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper om;
+
+    @Nested
+    class checkEmailTest {
+        @Test
+        @DisplayName("성공 : 이메일 중복 체크")
+        void checkEmailTest() throws Exception {
+
+            // given
+            CheckEmailRequest request =
+                    CheckEmailRequest.builder().email("ekrrdj07@jnu.ac.kr").build();
+            String requestBody = om.writeValueAsString(request);
+            // when
+            ResultActions resultActions =
+                    mvc.perform(
+                            post("/v1/auth/check/email")
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .content(requestBody));
+            // eye
+            String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+            // then
+            resultActions.andExpectAll(
+                    status().isOk(),
+                    jsonPath("$.message").value(ResponseMessage.IS_POSSIBLE_EMAIL)
+            );
+            log.info("responseBody : " + responseBody);
+
+        }
+
+        @Test
+        @DisplayName("실패 : 이메일 중복 체크(이메일이 이미 존재하는 경우)")
+        void checkEmailTestFail() throws Exception {
+
+            // given
+            CheckEmailRequest request =
+                    CheckEmailRequest.builder().email("council@jnu.ac.kr").build();
+            String requestBody = om.writeValueAsString(request);
+            // when
+            ResultActions resultActions =
+                    mvc.perform(
+                            post("/v1/auth/check/email")
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .content(requestBody));
+            // eye
+            String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+            // then
+            resultActions.andExpectAll(
+                    status().is4xxClientError(),
+                    jsonPath("$.status").value(400),
+                    jsonPath("$.reason").value(CouncilErrorCode.ALREADY_EXIST_EMAIL.getReason())
+            );
+            log.info("responseBody : " + responseBody);
+        }
+
+        @Test
+        @DisplayName("실패 : 이메일 중복 체크(이메일이 이메일 형식에 맞지 않는경우)")
+        void loginTestFail3() throws Exception {
+
+            // given
+            CheckEmailRequest request =
+                    CheckEmailRequest.builder().email("ekrrrdj21jnu.ac.kr").build();
+            String requestBody = om.writeValueAsString(request);
+            // when
+            ResultActions resultActions =
+                    mvc.perform(
+                            post("/v1/auth/check/email")
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .content(requestBody));
+            // eye
+            String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+            // then
+            resultActions.andExpectAll(
+                    status().is4xxClientError(),
+                    jsonPath("$.status").value(400),
+                    jsonPath("$.reason").value(ValidationMessage.IS_NOT_VALID_EMAIL)
+            );
+            log.info("responseBody : " + responseBody);
+        }
+    }
+}
