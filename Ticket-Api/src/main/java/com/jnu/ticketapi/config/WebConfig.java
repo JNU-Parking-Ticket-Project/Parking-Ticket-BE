131a35c58f2df895b2f9c92b7db79b789c564845
Kwon770 <sckwon770@gmail.com>
2023-12-13 12:49:20 +0900

    feat: Allow Cors for deployed frontend domain

--- Ticket-Api/src/main/java/com/jnu/ticketapi/config/WebConfig.java
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/config/WebConfig.java
@@ -27,6 +27,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("https://apply.jnu-parking.com/")
+                .allowedOrigins("https://manager.jnu-parking.com/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .maxAge(3600);
