8a80df41ba1f8f4b264d434a42dc69342f05de93
Jin Hyuk <ekrrdj07@gmail.com>
2023-12-14 20:09:39 +0900

    [test] : 임시저장 조회 통합 테스트

--- Ticket-Api/src/main/resources/db/teardown.sql
+++ Ticket-Api/src/main/resources/db/teardown.sql
@@ -1,4 +1,5 @@
-SET foreign_key_checks = 0;
+SET
+foreign_key_checks = 0;
 
 truncate table announce_tb;
 truncate table captcha_tb;
@@ -10,13 +11,34 @@ truncate table registration_tb;
 truncate table sector;
 truncate table user_tb;
 
-SET foreign_key_checks = 1;
+SET
+foreign_key_checks = 1;
 
 
 insert into user_tb(user_id, email, email_confirmed, pwd, sequence, status, role)
--- council 비밀번호 Council@123, admin 비밀번호 Admin@123
-VALUES (1, 'council@jnu.ac.kr', false, '$2a$10$iH6JVDTpjdq0azNVVjmluu5jHy3NG92zoNjeA0x.EwLL.tTMIvUmq', -2, '불합격', 'USER'),
-       (2, 'admin@jnu.ac.kr', false, '$2a$10$I0J8oSZ.7Mq3itr1g5DnTeRAZQydvN1qp8CWck2dvxNOnO.bPrKhK', -2, '불합격', 'ADMIN');
+-- council 비밀번호 Council@123, admin 비밀번호 Admin@123, user 비밀번호 User@1234
+VALUES (1, 'council@jnu.ac.kr', false, '$2a$10$iH6JVDTpjdq0azNVVjmluu5jHy3NG92zoNjeA0x.EwLL.tTMIvUmq', -2, '불합격',
+        'USER'),
+       (2, 'admin@jnu.ac.kr', false, '$2a$10$I0J8oSZ.7Mq3itr1g5DnTeRAZQydvN1qp8CWck2dvxNOnO.bPrKhK', -2, '불합격',
+        'ADMIN'),
+       (3, 'user@jnu.ac.kr', false, '$$2a$10$S6jHnlYjhNDGeQd8HXbxjONA0U15TT.g.FjMwOCTok4YOxx6A8C7y', -2, '불합격',
+        'USER');
 insert into council_tb(id, name, phone_num, student_num, user_id)
 VALUES (1, '학생회장', '010-1234-5678', '2019101234', 1);
-update user_tb set role = 'COUNCIL' where user_id = 1;
+update user_tb
+set role = 'COUNCIL'
+where user_id = 1;
+
+insert into event(event_id, event_code, event_status, end_at, start_at)
+values (1, '596575', 'OPEN', TIMESTAMPADD(MINUTE, 30, current_time), TIMESTAMPADD(MINUTE, -30, current_time));
+
+insert into sector(sector_id, issue_amount, name, remaining_amount, reserve, sector_capacity, sector_number, event_id)
+values (1, 40, '사회대 / 농대 / 수의대 / 치전원', 40, 0, 40, '1구간', 1),
+       (2, 15, '경영대 / 인문대', 15, 0, 15, '2구간', 1),
+       (3, 45, '사범대 / 예술대 / AI융합대 / 본부직할', 45, 0, 45, '3구간', 1),
+       (4, 70, '공대 / 간호-의대 1학년', 70, 0, 70, '4구간', 1),
+       (5, 30, '자연대 / 약대 / 생활대', 30, 0, 30, '5구간', 1);
+
+insert into registration_tb(id, affiliation, car_num, created_at, email, is_light, is_saved, name, student_num, phone_num, sector_id, user_id, is_deleted)
+values (1, '공과대학', '1234가1234', current_time, 'user@jnu.ac.kr', true, true, '이진혁', '215555', '010-000-0000', 4, 3, false),
+       (2, '경영대학', '가1234', current_time, 'council@jnu.ac.kr', true, false, '박영규', '192155','010-000-0000', 4, 1, false);
---
+++ Ticket-Api/src/test/java/com/jnu/ticketapi/registration/GetRegistrationTest.java
@@ -0,0 +1,92 @@
+package com.jnu.ticketapi.registration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jnu.ticketapi.security.JwtGenerator;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Slf4j
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@Sql("classpath:db/teardown.sql")
+public class GetRegistrationTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper om;
+
+    @Autowired
+    JwtGenerator jwtGenerator;
+
+    @Nested
+    class getRegistrationTest {
+
+        @Test
+        @DisplayName("성공 : 임시저장 조회(임시저장을 안했을 경우)")
+        void getRegistrationTest() throws Exception {
+            // given
+            String email = "admin@jnu.ac.kr";
+            String accessToken = jwtGenerator.generateAccessToken(email, "ADMIN");
+
+            // when
+            ResultActions resultActions =
+                    mvc.perform(
+                            get("/v1/registration")
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .header("Authorization", "Bearer " + accessToken));
+            // eye
+            String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+
+            // then
+            resultActions.andExpectAll(
+                    status().isOk(),
+                    jsonPath("$.email").value(email),
+                    jsonPath("$.studentNum").isEmpty(),
+                    jsonPath("$.phoneNum").isEmpty()
+            );
+            log.info("responseBody : {}", responseBody);
+        }
+
+        @Test
+        @DisplayName("성공 : 임시저장 조회(임시저장을 했을 경우)")
+        void getRegistrationTest2() throws Exception {
+            // given
+            String email = "user@jnu.ac.kr";
+            String accessToken = jwtGenerator.generateAccessToken(email, "USER");
+
+            // when
+            ResultActions resultActions =
+                    mvc.perform(
+                            get("/v1/registration")
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .header("Authorization", "Bearer " + accessToken));
+            // eye
+            String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+
+            // then
+            resultActions.andExpectAll(
+                    status().isOk(),
+                    jsonPath("$.email").value(email),
+                    jsonPath("$.name").value("이진혁"),
+                    jsonPath("$.studentNum").value("215555")
+            );
+            log.info("responseBody : {}", responseBody);
+        }
+    }
+}
