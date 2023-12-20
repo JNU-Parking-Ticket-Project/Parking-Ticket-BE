1e4f397fa1040e4b9032ed9f08cfcf052fa74965
Cookie <pon05114@naver.com>
2023-12-20 19:01:04 +0900

    [hotfix] : 비밀번호 재설정 기능 이메일, 비밀번호 Valid 추가

--- Ticket-Api/src/main/java/com/jnu/ticketapi/api/user/model/request/FindPasswordRequest.java
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/api/user/model/request/FindPasswordRequest.java
@@ -1,10 +1,13 @@
 package com.jnu.ticketapi.api.user.model.request;
 
 
+import com.jnu.ticketcommon.message.ValidationMessage;
 import com.jnu.ticketdomain.domains.CredentialCode.domain.CredentialCode;
+import javax.validation.constraints.Email;
 import lombok.Builder;
 
-public record FindPasswordRequest(String email) {
+public record FindPasswordRequest(
+        @Email(message = ValidationMessage.IS_NOT_VALID_EMAIL) String email) {
     @Builder
     public FindPasswordRequest {}
 
