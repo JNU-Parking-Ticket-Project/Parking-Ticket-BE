9a200e845cbb511e2526e92a564169973a1209c1
Jin Hyuk <ekrrdj07@gmail.com>
2023-12-22 00:29:37 +0900

    [style] : spotless

--- Ticket-Api/src/main/java/com/jnu/ticketapi/api/user/controller/UserController.java
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/api/user/controller/UserController.java
@@ -12,11 +12,10 @@ import com.jnu.ticketcommon.annotation.ApiErrorExceptionsExample;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
 
 @SecurityRequirement(name = "access-token")
 @RestController
@@ -38,7 +37,8 @@ public class UserController {
     @PostMapping("/user/update/password/{code}")
     @ApiErrorExceptionsExample(UpdatePasswordExceptionDocs.class)
     public ResponseEntity<UpdatePasswordResponse> updatePassword(
-            @PathVariable String code, @RequestBody @Valid UpdatePasswordRequest updatePasswordRequest) {
+            @PathVariable String code,
+            @RequestBody @Valid UpdatePasswordRequest updatePasswordRequest) {
 
         return ResponseEntity.ok(updatePasswordUseCase.execute(code, updatePasswordRequest));
     }
