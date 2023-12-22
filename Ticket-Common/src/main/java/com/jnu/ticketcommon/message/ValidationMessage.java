f78c2a8115f3b36087780c2777046fc312720b7e
Jin Hyuk <ekrrdj07@gmail.com>
2023-12-22 20:06:16 +0900

    [fix] : ValidationMessage
    - 유효하지 않는 비밀번호 예외 메세지 수정

--- Ticket-Common/src/main/java/com/jnu/ticketcommon/message/ValidationMessage.java
+++ Ticket-Common/src/main/java/com/jnu/ticketcommon/message/ValidationMessage.java
@@ -6,7 +6,7 @@ public class ValidationMessage {
     public static final String MUST_NOT_BLANK = "null 혹은 빈 값이 들어올 수 없습니다.";
 
     public static final String IS_NOT_VALID_PASSWORD =
-            "비밀번호는 최소 8자 이상이며, 최소 하나의 대문자, 소문자, 숫자, 특수 문자를 포함해야 합니다.";
+            "비밀번호는 최소 8자 이상 최대 20자 이하이며, 최소 하나의 대문자, 소문자, 숫자, 특수 문자를 포함해야 합니다.";
 
     public static final String IS_NOT_VALID_EMAIL = "이메일 형식이 올바르지 않습니다.";
 
