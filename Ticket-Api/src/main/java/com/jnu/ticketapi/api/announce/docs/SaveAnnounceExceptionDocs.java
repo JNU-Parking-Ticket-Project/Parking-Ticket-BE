7e8a7b2ebf184f733b7f3a956d7e0a73fde9090d
Cookie <pon05114@naver.com>
2023-12-31 05:44:20 +0900

    [hotfix] : 안내사항, 공지사항 swagger 업데이트

--- Ticket-Api/src/main/java/com/jnu/ticketapi/api/announce/docs/SaveAnnounceExceptionDocs.java
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/api/announce/docs/SaveAnnounceExceptionDocs.java
@@ -2,7 +2,18 @@ package com.jnu.ticketapi.api.announce.docs;
 
 
 import com.jnu.ticketcommon.annotation.ExceptionDoc;
+import com.jnu.ticketcommon.annotation.ExplainError;
+import com.jnu.ticketcommon.exception.TicketCodeException;
 import com.jnu.ticketcommon.interfaces.SwaggerExampleExceptions;
+import com.jnu.ticketdomain.domains.announce.exception.AnnounceErrorCode;
 
 @ExceptionDoc
-public class SaveAnnounceExceptionDocs implements SwaggerExampleExceptions {}
+public class SaveAnnounceExceptionDocs implements SwaggerExampleExceptions {
+    @ExplainError("공지사항 내용이 10,000자를 초과하는 경우")
+    public TicketCodeException 공지사항_내용이_10000자를_초과함 =
+            new TicketCodeException(AnnounceErrorCode.ANNOUNCE_CONTENT_EXCEEDS_LIMIT_ERROR);
+
+    @ExplainError("공지사항 제목이 100자를 초과하는 경우")
+    public TicketCodeException 공지사항_제목이_100자를_초과함 =
+            new TicketCodeException(AnnounceErrorCode.ANNOUNCE_TITLE_EXCEEDS_LIMIT_ERROR);
+}
