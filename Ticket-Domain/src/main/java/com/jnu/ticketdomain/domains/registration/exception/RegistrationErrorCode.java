90488e9aeacefd9dbbd773f8ed7a2a7547d0a816
Jin Hyuk <ekrrdj07@gmail.com>
2024-01-02 20:09:12 +0900

    [style] : 예외 응답 코드 수정
    - BAD_REQUEST로 수정

--- Ticket-Domain/src/main/java/com/jnu/ticketdomain/domains/registration/exception/RegistrationErrorCode.java
+++ Ticket-Domain/src/main/java/com/jnu/ticketdomain/domains/registration/exception/RegistrationErrorCode.java
@@ -7,6 +7,8 @@ import com.jnu.ticketcommon.exception.BaseErrorCode;
 import com.jnu.ticketcommon.exception.ErrorReason;
 import java.lang.reflect.Field;
 import java.util.Objects;
+
+import com.jnu.ticketcommon.message.ValidationMessage;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -14,7 +16,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum RegistrationErrorCode implements BaseErrorCode {
     NOT_FOUND_REGISTRATION(NOT_FOUND, "REGISTRATION_404_1", "존재하지 않는 등록 신청 입니다."),
-    ALREADY_EXIST_REGISTRATION(400, "REGISTRATION_400_1", "이미 등록 신청을 하셨습니다.");
+    ALREADY_EXIST_REGISTRATION(400, "REGISTRATION_400_1", "이미 등록 신청을 하셨습니다."),
+    AFFILIATION_MUST_NOT_BLANK(400, "BAD_REQUEST", "소속대학을 " + ValidationMessage.MUST_NOT_BLANK),
+    ;
     private final Integer status;
     private final String code;
     private final String reason;
