7fa9c5ef5905431184637f2770b767b76b718bd7
blackbean <ymecca730135@gmail.com>
2024-01-19 00:57:10 +0900

    [feat] : Sector 이름 중복 검증 Vavr Validation

--- Ticket-Api/src/main/java/com/jnu/ticketapi/api/sector/service/SectorRegisterUseCase.java
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/api/sector/service/SectorRegisterUseCase.java
@@ -4,12 +4,16 @@ package com.jnu.ticketapi.api.sector.service;
 import com.jnu.ticketapi.api.sector.model.request.SectorRegisterRequest;
 import com.jnu.ticketapi.api.sector.model.response.SectorReadResponse;
 import com.jnu.ticketcommon.annotation.UseCase;
+import com.jnu.ticketcommon.exception.TicketCodeException;
 import com.jnu.ticketdomain.common.aop.event.EventTypeCheck;
 import com.jnu.ticketdomain.domains.events.domain.EventStatus;
 import com.jnu.ticketdomain.domains.events.domain.Sector;
+import com.jnu.ticketdomain.domains.events.exception.DuplicateSectorNameException;
 import com.jnu.ticketdomain.domains.events.out.EventLoadPort;
 import com.jnu.ticketdomain.domains.events.out.SectorLoadPort;
 import com.jnu.ticketdomain.domains.events.out.SectorRecordPort;
+import io.vavr.control.Validation;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,24 +28,72 @@ public class SectorRegisterUseCase {
     private final SectorLoadPort sectorLoadPort;
     private final EventLoadPort eventLoadPort;
 
+    //    @Transactional
+    //    public void execute(List<SectorRegisterRequest> sectors) {
+    //        // 구간별 이름 중복 체그
+    //        Validation result = validateRegistrationSector(sectors);
+    //        List<Sector> sectorList = sectors.stream()
+    //                .map(
+    //                    sectorRegisterRequest ->
+    //                        new Sector(
+    //                            sectorRegisterRequest.sectorNumber(),
+    //                            sectorRegisterRequest.name(),
+    //                            sectorRegisterRequest.sectorCapacity(),
+    //                            sectorRegisterRequest.reserve()))
+    //                .toList();
+    //        sectorRecordPort.saveAll(sectorList);
+    //    }
     @Transactional
     public void execute(List<SectorRegisterRequest> sectors) {
-        // to Sector List
-        List<Sector> sectorList =
-                sectors.stream()
-                        .map(
-                                sectorRegisterRequest ->
-                                        new Sector(
-                                                sectorRegisterRequest.sectorNumber(),
-                                                sectorRegisterRequest.name(),
-                                                sectorRegisterRequest.sectorCapacity(),
-                                                sectorRegisterRequest.reserve()))
-                        .toList();
-        sectorRecordPort.saveAll(sectorList);
+        // Check for duplicate sector names
+        Validation<TicketCodeException, io.vavr.collection.List<SectorRegisterRequest>> result =
+                validateRegistrationSector(sectors);
+
+        result.fold(
+                validationError -> {
+                    throw validationError;
+                },
+                validSectors -> {
+                    List<Sector> sectorList =
+                            validSectors
+                                    .map(
+                                            sectorRegisterRequest ->
+                                                    new Sector(
+                                                            sectorRegisterRequest.sectorNumber(),
+                                                            sectorRegisterRequest.name(),
+                                                            sectorRegisterRequest.sectorCapacity(),
+                                                            sectorRegisterRequest.reserve()))
+                                    .toJavaList();
+                    sectorRecordPort.saveAll(sectorList);
+                    return null;
+                });
+    }
+
+    private Validation<TicketCodeException, io.vavr.collection.List<SectorRegisterRequest>>
+            validateRegistrationSector(List<SectorRegisterRequest> sectors) {
+        //        return Validation.combine(validateUniqueSectorNumbers(sectors)
+        ////            , validateLogic2()
+        ////            , validateLogic3()
+        //            )
+        //            .ap((uniqueSectorNumbers) -> io.vavr.collection.List.ofAll(sectors));
+        return validateUniqueSectorNumbers(sectors)
+                .flatMap(
+                        uniqueSectorNumbers ->
+                                Validation.valid(io.vavr.collection.List.ofAll(sectors)));
+    }
+
+    private Validation<TicketCodeException, io.vavr.collection.List<String>>
+            validateUniqueSectorNumbers(java.util.List<SectorRegisterRequest> sectors) {
+        java.util.List<String> sectorNumbers = new ArrayList<>();
+        sectors.forEach(sector -> sectorNumbers.add(sector.sectorNumber()));
+
+        return sectorNumbers.stream().distinct().count() == sectorNumbers.size()
+                ? Validation.valid(io.vavr.collection.List.ofAll(sectorNumbers))
+                : Validation.invalid(DuplicateSectorNameException.EXCEPTION);
     }
 
     @Transactional
-    @EventTypeCheck(eventType = EventStatus.OPEN)
+    @EventTypeCheck(eventType = EventStatus.READY)
     public void update(List<SectorRegisterRequest> sectors) {
         List<Sector> prevSector = sectorLoadPort.findAll();
         List<Sector> sectorList =
