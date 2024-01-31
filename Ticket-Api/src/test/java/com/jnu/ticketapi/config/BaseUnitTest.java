218ec39d1259a52e3526354a6041b4894a065a2e
Cookie <pon05114@naver.com>
2024-01-31 18:41:45 +0900

    [test] :이벤트 PUBLISH 상태조회 통합테스트

---
+++ Ticket-Api/src/test/java/com/jnu/ticketapi/config/BaseUnitTest.java
@@ -0,0 +1,9 @@
+package com.jnu.ticketapi.config;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class BaseUnitTest {
+
+}
