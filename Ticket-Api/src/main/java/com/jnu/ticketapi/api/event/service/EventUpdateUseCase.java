9fbcaac89d1ce85d4515921d69ce8f5f4ebd62cc
Jin Hyuk <ekrrdj07@gmail.com>
2024-02-26 22:54:41 +0900

    [fix] : validateEventIssuePeriod 수정

--- Ticket-Api/src/main/java/com/jnu/ticketapi/api/event/service/EventUpdateUseCase.java
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/api/event/service/EventUpdateUseCase.java
@@ -8,18 +8,23 @@ import com.jnu.ticketdomain.domains.events.domain.Event;
 import com.jnu.ticketdomain.domains.events.domain.EventStatus;
 import com.jnu.ticketdomain.domains.events.exception.CannotUpdateClosedEventException;
 import com.jnu.ticketdomain.domains.events.exception.CannotUpdatePublishEventException;
+import com.jnu.ticketdomain.domains.events.exception.InvalidPeriodEventException;
 import io.vavr.collection.Seq;
 import io.vavr.control.Validation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @UseCase
 @RequiredArgsConstructor
 public class EventUpdateUseCase {
     private final EventAdaptor eventAdaptor;
 
+
     @Transactional
     public void updateEvent(EventUpdateRequest eventUpdateRequest, Long eventId) {
+        validateEventIssuePeriod(eventUpdateRequest);
         Event event = eventAdaptor.findById(eventId);
         Validation<Seq<TicketCodeException>, Event> result = validateUpdateEvent(event);
         result.fold(
@@ -33,10 +38,9 @@ public class EventUpdateUseCase {
     Validation<Seq<TicketCodeException>, Event> validateUpdateEvent(Event event) {
         return Validation.combine(
                         validateEventStatusIsClosed(event),
-                        validateEventPublishIsTrue(event),
-                        validateEventIssuePeriod(event))
+                        validateEventPublishIsTrue(event))
                 .ap(
-                        (eventStatus, eventPublish, eventIssuePeriod) -> event);
+                        (eventStatus, eventPublish) -> event);
     }
 
 
@@ -52,8 +56,11 @@ public class EventUpdateUseCase {
         return Validation.valid(event);
     }
 
-    private Validation<TicketCodeException, Event> validateEventIssuePeriod(Event event) {
-        event.validateIssuePeriod();
-        return Validation.valid(event);
+    private void validateEventIssuePeriod(EventUpdateRequest request) {
+        LocalDateTime nowTime = LocalDateTime.now();
+        if (request.dateTimePeriod().getEndAt().isBefore(nowTime)
+                || request.dateTimePeriod().getEndAt().isBefore(request.dateTimePeriod().getStartAt())) {
+            throw InvalidPeriodEventException.EXCEPTION;
+        }
     }
 }
