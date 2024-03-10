dd184e4714404dcd647bfc987441e0667a019b3b
blackbean <ymecca730135@gmail.com>
2024-03-11 02:14:52 +0900

    [feat]: Event Deletion Handler

--- Ticket-Api/src/main/java/com/jnu/ticketapi/api/event/service/EventDeleteUseCase.java
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/api/event/service/EventDeleteUseCase.java
@@ -2,8 +2,10 @@ package com.jnu.ticketapi.api.event.service;
 
 
 import com.jnu.ticketcommon.annotation.UseCase;
+import com.jnu.ticketdomain.common.domainEvent.Events;
 import com.jnu.ticketdomain.domains.events.adaptor.EventAdaptor;
 import com.jnu.ticketdomain.domains.events.domain.Event;
+import com.jnu.ticketdomain.domains.events.event.EventDeletedEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +17,7 @@ public class EventDeleteUseCase {
     @Transactional
     public void deleteEvent(Long eventId) {
         Event event = eventAdaptor.findById(eventId);
+        Events.raise(EventDeletedEvent.of(event));
         event.deleteEvent();
     }
 }
