f4add5ba96d1739cc0238f616aad6142255f7c6f
Jin Hyuk <ekrrdj07@gmail.com>
2024-03-13 16:44:16 +0900

    [fix] : EventRegisterJob, EventUpdateJob
    - Quartz의 withIdentity에서 eventId 제거

--- Ticket-Batch/src/main/java/com/jnu/ticketbatch/job/EventUpdateJob.java
+++ Ticket-Batch/src/main/java/com/jnu/ticketbatch/job/EventUpdateJob.java
@@ -57,9 +57,9 @@ public class EventUpdateJob implements Job {
             Scheduler sched = schedFact.getScheduler();
 
             // JobKey 생성
-            JobKey jobKey1 = JobKey.jobKey("RESERVATION_JOB" + eventId, "group1");
+            JobKey jobKey1 = JobKey.jobKey("RESERVATION_JOB", "group1");
             log.info(">>>>> 예약 생성 작업 스케줄러에서 삭제");
-            JobKey jobKey2 = JobKey.jobKey("EXPIRED_JOB" + eventId, "group1");
+            JobKey jobKey2 = JobKey.jobKey("EXPIRED_JOB", "group1");
             log.info(">>>>> 만료 작업 스케줄러에서 삭제");
             // 스케줄러에서 작업 삭제
             if (sched.checkExists(jobKey1)) { // 해당 JobKey로 등록된 작업이 존재하는지 확인
@@ -85,7 +85,7 @@ public class EventUpdateJob implements Job {
 
         JobDetail reserveEventQuartzJob =
                 newJob(QuartzJobLauncher.class)
-                        .withIdentity("RESERVATION_JOB" + eventId, "group1")
+                        .withIdentity("RESERVATION_JOB", "group1")
                         .usingJobData("eventId", eventId) // Pass eventId as job data
                         .setJobData(jobDataMap)
                         .build();
