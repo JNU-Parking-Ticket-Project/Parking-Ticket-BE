d9eb45d8f8327bc044453a16344542aed8cde083
Jin Hyuk <ekrrdj07@gmail.com>
2024-04-12 23:09:11 +0900

    [style] : spotless

--- Ticket-Common/src/main/java/com/jnu/ticketcommon/config/ModuleConfig.java
+++ Ticket-Common/src/main/java/com/jnu/ticketcommon/config/ModuleConfig.java
@@ -5,11 +5,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ModuleConfig {
@@ -20,9 +19,9 @@ public class ModuleConfig {
         objectMapper.registerModule(new JavaTimeModule());
         JavaTimeModule module = new JavaTimeModule();
         // Define the pattern for LocalDateTime
-        LocalDateTimeSerializer localDateTimeSerializer = new LocalDateTimeSerializer(
-                DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS")
-        );
+        LocalDateTimeSerializer localDateTimeSerializer =
+                new LocalDateTimeSerializer(
+                        DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS"));
         module.addSerializer(LocalDateTime.class, localDateTimeSerializer);
 
         ObjectMapper mapper = new ObjectMapper();
