744ba6c4527e330fb6513747d1bbf920a62ba078
Jin Hyuk <ekrrdj07@gmail.com>
2024-04-12 23:08:56 +0900

    [feat] : LocalDateTime 전역적으로 pattern 적용

--- Ticket-Common/src/main/java/com/jnu/ticketcommon/config/ModuleConfig.java
+++ Ticket-Common/src/main/java/com/jnu/ticketcommon/config/ModuleConfig.java
@@ -2,9 +2,14 @@ package com.jnu.ticketcommon.config;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Configuration
 public class ModuleConfig {
@@ -13,6 +18,18 @@ public class ModuleConfig {
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
-        return objectMapper;
+        JavaTimeModule module = new JavaTimeModule();
+        // Define the pattern for LocalDateTime
+        LocalDateTimeSerializer localDateTimeSerializer = new LocalDateTimeSerializer(
+                DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS")
+        );
+        module.addSerializer(LocalDateTime.class, localDateTimeSerializer);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(module);
+        // Write dates as strings instead of timestamps
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        return mapper;
     }
 }
