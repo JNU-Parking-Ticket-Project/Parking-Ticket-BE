237c14ce060ac0534098db3f4b42a57cdb4922b2
Jin Hyuk <ekrrdj07@gmail.com>
2024-07-24 14:12:53 +0900

    spotless

--- Ticket-Domain/src/test/java/com/jnu/ticketdomain/DomainIntegrateTestConfig.java
+++ Ticket-Domain/src/test/java/com/jnu/ticketdomain/DomainIntegrateTestConfig.java
@@ -7,9 +7,5 @@ import org.springframework.context.annotation.Configuration;
 
 /** 스프링 부트 설정의 컴포넌트 스캔범위를 지정 통합 테스트를 위함 */
 @Configuration
-@ComponentScan(
-        basePackageClasses = {
-            TicketDomainApplication.class,
-            TicketCommonApplication.class
-        })
+@ComponentScan(basePackageClasses = {TicketDomainApplication.class, TicketCommonApplication.class})
 public class DomainIntegrateTestConfig {}
