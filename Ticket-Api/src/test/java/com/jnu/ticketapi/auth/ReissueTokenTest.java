e701f78daf830d8e764cdf9259c0f3a595ef1867
Jin Hyuk <ekrrdj07@gmail.com>
2024-07-24 14:20:18 +0900

    [build] import MockMvcRequestBuilders.post

--- Ticket-Api/src/test/java/com/jnu/ticketapi/auth/ReissueTokenTest.java
+++ Ticket-Api/src/test/java/com/jnu/ticketapi/auth/ReissueTokenTest.java
@@ -1,5 +1,6 @@
 package com.jnu.ticketapi.auth;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
