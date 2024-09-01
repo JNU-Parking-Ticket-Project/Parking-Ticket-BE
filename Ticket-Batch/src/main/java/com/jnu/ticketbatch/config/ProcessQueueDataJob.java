69f5483f9abb13c074d77fb4836eeab42e9a2f07
Jin Hyuk <ekrrdj07@gmail.com>
2024-09-02 07:02:13 +0900

    [fix] : redis 대기열에서 pop & DB에 Save 하지 못하는 오류 해결

--- Ticket-Batch/src/main/java/com/jnu/ticketbatch/config/ProcessQueueDataJob.java
+++ Ticket-Batch/src/main/java/com/jnu/ticketbatch/config/ProcessQueueDataJob.java
@@ -26,25 +26,18 @@ public class ProcessQueueDataJob implements Job {
 
             WaitingQueueService waitingQueueService =
                     (WaitingQueueService) jobDataMap.get("waitingQueueService");
-            ChatMessage message =
-                    waitingQueueService.getValueByStatus(
-                            REDIS_EVENT_ISSUE_STORE, ChatMessageStatus.NOT_WAITING);
+            ChatMessage message = (ChatMessage) waitingQueueService.findFirst(REDIS_EVENT_ISSUE_STORE);
 
             if (message != null) {
                 log.info("Message found, raising event");
                 Double score = waitingQueueService.getScore(REDIS_EVENT_ISSUE_STORE, message);
                 waitingQueueService.reRegisterQueue(
                         REDIS_EVENT_ISSUE_STORE, message, ChatMessageStatus.WAITING, score);
-                Events.raise(
-                        EventIssuedEvent.from(
-                                message.getSectorId(),
-                                message.getUserId(),
-                                message.getEventId(),
-                                message.getRegistration(),
-                                score));
+                Events.raise(EventIssuedEvent.from(message, score));
             }
         } catch (Exception e) {
             log.error("ProcessQueueDataJob Exception: {}", e.getMessage());
         }
     }
+
 }
