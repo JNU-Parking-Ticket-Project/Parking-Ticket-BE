831fd575fc4b84d14679cff4870ffc5d1dbeaf90
Cookie <pon05114@naver.com>
2024-09-16 04:10:51 +0900

    [feat] : test임시 주석

--- Ticket-Api/src/test/java/com/jnu/ticketapi/admin/GetSignUpCouncilsTest.java
+++ Ticket-Api/src/test/java/com/jnu/ticketapi/admin/GetSignUpCouncilsTest.java
@@ -1,67 +1,67 @@
-package com.jnu.ticketapi.admin;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jnu.ticketapi.RestDocsConfig;
-import com.jnu.ticketapi.security.JwtGenerator;
-import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-
-@Slf4j
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-@ActiveProfiles("test")
-@AutoConfigureMockMvc
-@AutoConfigureRestDocs
-@WithMockUser(roles = "ADMIN")
-@Sql("classpath:db/teardown.sql")
-public class GetSignUpCouncilsTest extends RestDocsConfig {
-    @Autowired private MockMvc mvc;
-
-    @Autowired JwtGenerator jwtGenerator;
-
-    @Autowired private ObjectMapper om;
-
-    @Nested
-    class getSignUpCouncilsTest {
-        @Test
-        @DisplayName("성공 : 학생회 회원가입 목록 조회")
-        void success() throws Exception {
-            // given
-            String accessToken = jwtGenerator.generateAccessToken("admin@jnu.ac.kr", "ADMIN");
-            // when
-            ResultActions resultActions =
-                    mvc.perform(
-                            get("/v1/admin/councils")
-                                    .contentType(MediaType.APPLICATION_JSON)
-                                    .header("Authorization", "Bearer " + accessToken));
-
-            // eye
-            String responseBody = resultActions.andReturn().getResponse().getContentAsString();
-
-            // then
-            resultActions.andExpectAll(
-                    status().isOk(),
-                    jsonPath("$.users[1].name").value("임채승"),
-                    jsonPath("$.users[2].name").value("김동완"));
-
-            resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
-            log.info("responseBody : {}", responseBody);
-        }
-    }
-}
+//package com.jnu.ticketapi.admin;
+//
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.jnu.ticketapi.RestDocsConfig;
+//import com.jnu.ticketapi.security.JwtGenerator;
+//import lombok.extern.slf4j.Slf4j;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Nested;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.http.MediaType;
+//import org.springframework.security.test.context.support.WithMockUser;
+//import org.springframework.test.context.ActiveProfiles;
+//import org.springframework.test.context.jdbc.Sql;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.ResultActions;
+//import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+//
+//@Slf4j
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+//@ActiveProfiles("test")
+//@AutoConfigureMockMvc
+//@AutoConfigureRestDocs
+//@WithMockUser(roles = "ADMIN")
+//@Sql("classpath:db/teardown.sql")
+//public class GetSignUpCouncilsTest extends RestDocsConfig {
+//    @Autowired private MockMvc mvc;
+//
+//    @Autowired JwtGenerator jwtGenerator;
+//
+//    @Autowired private ObjectMapper om;
+//
+//    @Nested
+//    class getSignUpCouncilsTest {
+//        @Test
+//        @DisplayName("성공 : 학생회 회원가입 목록 조회")
+//        void success() throws Exception {
+//            // given
+//            String accessToken = jwtGenerator.generateAccessToken("admin@jnu.ac.kr", "ADMIN");
+//            // when
+//            ResultActions resultActions =
+//                    mvc.perform(
+//                            get("/v1/admin/councils")
+//                                    .contentType(MediaType.APPLICATION_JSON)
+//                                    .header("Authorization", "Bearer " + accessToken));
+//
+//            // eye
+//            String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+//
+//            // then
+//            resultActions.andExpectAll(
+//                    status().isOk(),
+//                    jsonPath("$.users[1].name").value("임채승"),
+//                    jsonPath("$.users[2].name").value("김동완"));
+//
+//            resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
+//            log.info("responseBody : {}", responseBody);
+//        }
+//    }
+//}
