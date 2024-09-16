da43ddb99cf9108a1b550e175aec391bc3eaaa86
Jin Hyuk <ekrrdj07@gmail.com>
2024-09-16 18:22:01 +0900

    [feat] : EventRegisterJob 반복되는 문자열 상수화

--- Ticket-Batch/src/main/java/com/jnu/ticketbatch/job/EventRegisterJob.java
+++ Ticket-Batch/src/main/java/com/jnu/ticketbatch/job/EventRegisterJob.java
@@ -1,28 +1,25 @@
 package com.jnu.ticketbatch.job;
 
-import static org.quartz.JobBuilder.newJob;
-import static org.quartz.TriggerBuilder.newTrigger;
-
 import com.jnu.ticketbatch.config.ProcessQueueDataJob;
 import com.jnu.ticketbatch.config.QuartzJobLauncher;
 import com.jnu.ticketbatch.expired.BatchQuartzJob;
 import com.jnu.ticketdomain.domains.events.EventExpiredEventRaiseGateway;
 import com.jnu.ticketinfrastructure.service.WaitingQueueService;
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.*;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.Configuration;
+
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
-import lombok.extern.slf4j.Slf4j;
-import org.quartz.*;
-import org.quartz.impl.StdSchedulerFactory;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.context.annotation.Configuration;
+
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.TriggerBuilder.newTrigger;
 
 @Configuration
 @Slf4j
@@ -32,16 +29,20 @@ public class EventRegisterJob implements Job {
     @Autowired private org.springframework.batch.core.Job expirationJob;
     @Autowired private EventExpiredEventRaiseGateway eventExpiredEventRaiseGateway;
     @Autowired private ApplicationEventPublisher applicationEventPublisher;
-    @Autowired(required = false)
-    private WaitingQueueService waitingQueueService;
+    @Autowired(required = false) private WaitingQueueService waitingQueueService;
     @Autowired private Scheduler scheduler;
+
+    private static final String EVENT_ID = "eventId";
+    private static final String GROUP = "group1";
+    private static final String ASIA_SEOUL = "Asia/Seoul";
+
 
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
         try {
             JobParameters jobParameters =
                     new JobParametersBuilder()
-                            .addLong("eventId", context.getMergedJobDataMap().getLong("eventId"))
+                            .addLong(EVENT_ID, context.getMergedJobDataMap().getLong(EVENT_ID))
                             .toJobParameters();
 
             jobLauncher.run(expirationJob, jobParameters);
@@ -54,19 +55,19 @@ public class EventRegisterJob implements Job {
         scheduler.start();
 
         JobDataMap jobDataMap = new JobDataMap();
-        jobDataMap.put("eventId", eventId);
+        jobDataMap.put(EVENT_ID, eventId);
         JobDetail reserveEventQuartzJob =
                 newJob(QuartzJobLauncher.class)
-                        .withIdentity("RESERVATION_JOB", "group1")
-                        .usingJobData("eventId", eventId) // Pass eventId as job data
+                        .withIdentity("RESERVATION_JOB", GROUP)
+                        .usingJobData(EVENT_ID, eventId) // Pass eventId as job data
                         .setJobData(jobDataMap)
                         .build();
 
-        Date date = Date.from(startAt.atZone(ZoneId.of("Asia/Seoul")).toInstant());
+        Date date = Date.from(startAt.atZone(ZoneId.of(ASIA_SEOUL)).toInstant());
 
         Trigger reserveTrigger =
                 newTrigger()
-                        .withIdentity("RESERVATION_TRIGGER", "group1")
+                        .withIdentity("RESERVATION_TRIGGER", GROUP)
                         .startAt(date)
                         .forJob(reserveEventQuartzJob)
                         .build();
@@ -80,19 +81,18 @@ public class EventRegisterJob implements Job {
         scheduler.start();
 
         JobDataMap jobDataMap = new JobDataMap();
-        jobDataMap.put("eventId", eventId);
+        jobDataMap.put(EVENT_ID, eventId);
         JobDetail expiredEventQuartzJob =
                 newJob(BatchQuartzJob.class)
-                        .withIdentity("EXPIRED_JOB", "group1")
-                        .usingJobData("eventId", eventId)//                .usingJobData("endAt", endAt.toString())
-                        .setJobData(jobDataMap)
+                        .withIdentity("EXPIRED_JOB", GROUP)
+                        .usingJobData(jobDataMap)//                .usingJobData("endAt", endAt.toString())
                         .build();
 
-        Date date = Date.from(endAt.atZone(ZoneId.of("Asia/Seoul")).toInstant());
+        Date date = Date.from(endAt.atZone(ZoneId.of(ASIA_SEOUL)).toInstant());
 
         Trigger reserveTrigger =
                 newTrigger()
-                        .withIdentity("EXPIRED_TRIGGER", "group1")
+                        .withIdentity("EXPIRED_TRIGGER", GROUP)
                         .startAt(date)
                         .forJob(expiredEventQuartzJob)
                         .build();
@@ -107,20 +107,20 @@ public class EventRegisterJob implements Job {
         scheduler.start();
 
         JobDataMap jobDataMap = new JobDataMap();
-        jobDataMap.put("eventId", eventId);
+        jobDataMap.put(EVENT_ID, eventId);
 
         JobDetail processQueueDataJob =
                 newJob(ProcessQueueDataJob.class)
-                        .withIdentity("PROCESS_QUEUE_DATA_JOB", "group1")
+                        .withIdentity("PROCESS_QUEUE_DATA_JOB", GROUP)
                         .usingJobData(jobDataMap) // eventId만 JobDataMap에 추가
                         .build();
 
-        Date start = Date.from(startAt.atZone(ZoneId.of("Asia/Seoul")).toInstant());
-        Date end = Date.from(endAt.atZone(ZoneId.of("Asia/Seoul")).toInstant());
+        Date start = Date.from(startAt.atZone(ZoneId.of(ASIA_SEOUL)).toInstant());
+        Date end = Date.from(endAt.atZone(ZoneId.of(ASIA_SEOUL)).toInstant());
 
         Trigger reserveTrigger =
                 newTrigger()
-                        .withIdentity("PROCESS_QUEUE_DATA_TRIGGER", "group1")
+                        .withIdentity("PROCESS_QUEUE_DATA_TRIGGER", GROUP)
                         .startAt(start)
                         .endAt(end)
                         .withSchedule(
