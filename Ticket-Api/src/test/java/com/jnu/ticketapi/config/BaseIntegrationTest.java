48e534ceb7708e48155e63c27a6c8f4d1a9ea99b
Jin Hyuk <ekrrdj07@gmail.com>
2024-09-27 22:14:05 +0900

    [fix] : BaseIntegrationTest 수정
    - @Sql 제거

--- Ticket-Api/src/test/java/com/jnu/ticketapi/config/BaseIntegrationTest.java
+++ Ticket-Api/src/test/java/com/jnu/ticketapi/config/BaseIntegrationTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 
 @Slf4j
@@ -15,6 +16,7 @@ import org.springframework.test.web.servlet.MockMvc;
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
 @ExtendWith(DatabaseClearExtension.class)
+@Sql("classpath:db/teardown.sql")
 public class BaseIntegrationTest {
 
     @Autowired protected MockMvc mockMvc;
