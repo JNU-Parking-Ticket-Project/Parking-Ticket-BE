4b9abbf66c6fc9a2aefddad214a190c2f93129c6
Jin Hyuk <ekrrdj07@gmail.com>
2024-09-27 22:05:59 +0900

    [fix] : teardown.sql 수정

--- Ticket-Api/src/main/resources/db/teardown.sql
+++ Ticket-Api/src/main/resources/db/teardown.sql
@@ -17,13 +17,13 @@ foreign_key_checks = 1;
 
 insert into user_tb(user_id, email, email_confirmed, pwd, sequence, status, role)
 -- council 비밀번호 Council@123, admin 비밀번호 Admin@123, user~3 비밀번호 User@1234
-VALUES (1, 'council@jnu.ac.kr', false, '$2a$10$iH6JVDTpjdq0azNVVjmluu5jHy3NG92zoNjeA0x.EwLL.tTMIvUmq', -2, '불합격',
+VALUES (1, 'council@jnu.ac.kr', false, '$2a$10$iH6JVDTpjdq0azNVVjmluu5jHy3NG92zoNjeA0x.EwLL.tTMIvUmq', -2, '합격',
         'COUNCIL'),
-       (2, 'admin@jnu.ac.kr', false, '$2a$10$I0J8oSZ.7Mq3itr1g5DnTeRAZQydvN1qp8CWck2dvxNOnO.bPrKhK', -2, '불합격',
+       (2, 'admin@jnu.ac.kr', false, '$2a$10$I0J8oSZ.7Mq3itr1g5DnTeRAZQydvN1qp8CWck2dvxNOnO.bPrKhK', -2, '합격',
         'ADMIN'),
-       (3, 'user@jnu.ac.kr', false, '$$2a$10$S6jHnlYjhNDGeQd8HXbxjONA0U15TT.g.FjMwOCTok4YOxx6A8C7y', -2, '불합격',
+       (3, 'user@jnu.ac.kr', false, '$$2a$10$S6jHnlYjhNDGeQd8HXbxjONA0U15TT.g.FjMwOCTok4YOxx6A8C7y', -2, '예비',
         'USER'),
-        (4, 'user2@jnu.ac.kr', false, '$$2a$10$S6jHnlYjhNDGeQd8HXbxjONA0U15TT.g.FjMwOCTok4YOxx6A8C7y', -2, '불합격',
+        (4, 'user2@jnu.ac.kr', false, '$$2a$10$S6jHnlYjhNDGeQd8HXbxjONA0U15TT.g.FjMwOCTok4YOxx6A8C7y', -2, '합격',
         'USER'),
        (5, 'user3@jnu.ac.kr', false, '$$2a$10$S6jHnlYjhNDGeQd8HXbxjONA0U15TT.g.FjMwOCTok4YOxx6A8C7y', -2, '불합격',
         'USER'),
@@ -44,7 +44,7 @@ insert into sector(sector_id, issue_amount, name, remaining_amount, reserve, sec
 values (1, 40, '사회대 / 농대 / 수의대 / 치전원', 40, 0, 40, '1구간', 1, 2, 2, false),
        (2, 15, '경영대 / 인문대', 15, 0, 15, '2구간', 1, 2, 2, false),
        (3, 45, '사범대 / 예술대 / AI융합대 / 본부직할', 45, 0, 45, '3구간', 1, 2, 2, false),
-       (4, 70, '공대 / 간호-의대 1학년', 70, 0, 70, '4구간', 1, 2, 2, false),
+       (4, 70, '공대 / 간호-의대 1학년', 70, 0, 70, '4구간', 1, 2, 3, false),
        (5, 30, '자연대 / 약대 / 생활대', 30, 0, 30, '5구간', 1, 2, 2, false);
 
 insert into registration_tb(id, affiliation, car_num, created_at, email, is_light, is_saved, name, student_num,
