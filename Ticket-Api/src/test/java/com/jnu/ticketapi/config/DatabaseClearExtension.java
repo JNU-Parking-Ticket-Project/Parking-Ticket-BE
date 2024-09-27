8aad78131c0bb082abc118c842674575a9d4b7d6
Jin Hyuk <ekrrdj07@gmail.com>
2024-09-27 22:14:42 +0900

    [fix] : DatabaseClearExtension 수정
    - AfterEach에서 BeforeEach로 다시 수정

--- Ticket-Api/src/test/java/com/jnu/ticketapi/config/DatabaseClearExtension.java
+++ Ticket-Api/src/test/java/com/jnu/ticketapi/config/DatabaseClearExtension.java
@@ -1,15 +1,14 @@
 package com.jnu.ticketapi.config;
 
 
-import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-public class DatabaseClearExtension implements AfterEachCallback {
+public class DatabaseClearExtension implements BeforeEachCallback {
 
     @Override
-    public void afterEach(ExtensionContext context) throws Exception {
+    public void beforeEach(ExtensionContext context) throws Exception {
         DatabaseCleaner databaseCleaner = getDataCleaner(context);
         databaseCleaner.clear();
     }
