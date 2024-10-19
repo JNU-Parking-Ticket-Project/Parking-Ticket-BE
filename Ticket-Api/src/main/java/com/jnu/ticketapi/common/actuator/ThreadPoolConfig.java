535560c41c8456f59c92c0012bf798c670b61076
Cookie <pon05114@naver.com>
2024-10-19 17:39:19 +0900

    [feat] : ThreadPoolConfig 수정

--- Ticket-Api/src/main/java/com/jnu/ticketapi/common/actuator/ThreadPoolConfig.java
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/common/actuator/ThreadPoolConfig.java
@@ -1,20 +1,37 @@
 package com.jnu.ticketapi.common.actuator;
 
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 
-@Component
 @Endpoint(id = "threadpool")
+@RequiredArgsConstructor
+@Component
 public class ThreadPoolConfig {
 
-    @Autowired private ThreadPoolTaskExecutor threadPoolTaskExecutor;
+    @Value("${thread.core-pool-size}")
+    private int corePoolSize;
+
+    @Value("${thread.max-pool-size}")
+    private int maxPoolSize;
+
+    @Value("${thread.queue-capacity}")
+    private int queueCapacity;
+
+    @Autowired @Lazy private ThreadPoolTaskExecutor threadPoolTaskExecutor;
 
     @ReadOperation
     public ThreadPoolStats threadPoolStats() {
+        threadPoolTaskExecutor.setCorePoolSize(corePoolSize);
+        threadPoolTaskExecutor.setMaxPoolSize(maxPoolSize);
+        threadPoolTaskExecutor.setQueueCapacity(queueCapacity);
+        threadPoolTaskExecutor.initialize();
         return new ThreadPoolStats(
                 threadPoolTaskExecutor.getPoolSize(),
                 threadPoolTaskExecutor.getActiveCount(),
