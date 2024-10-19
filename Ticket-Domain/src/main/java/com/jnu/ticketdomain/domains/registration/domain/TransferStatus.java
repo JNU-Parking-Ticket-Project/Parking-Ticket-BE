2e297e84d253bcb08ef024c12ee83735943ee6e5
Kwon770 <sckwon770@gmail.com>
2024-10-19 12:03:11 +0900

    chore: Add comment in TransferStatis ENUM

--- Ticket-Domain/src/main/java/com/jnu/ticketdomain/domains/registration/domain/TransferStatus.java
+++ Ticket-Domain/src/main/java/com/jnu/ticketdomain/domains/registration/domain/TransferStatus.java
@@ -1,6 +1,7 @@
 package com.jnu.ticketdomain.domains.registration.domain;
 
 public enum TransferStatus {
+    // 이메일 전송 대상의 조회 정렬 조건이므로, 순서를 변경하지 마세요.
     PENDING, // 전송 대기
     FAILED_1, // 1차 전송 실패
     FAILED_2,
