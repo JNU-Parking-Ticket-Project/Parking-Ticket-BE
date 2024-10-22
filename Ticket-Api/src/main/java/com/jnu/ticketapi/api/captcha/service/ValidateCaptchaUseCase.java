fc67bd050e5d78718c6c979a684f81545f3bf94f
kssumin <201566@jnu.ac.kr>
2024-10-23 00:50:35 +0900

    feat : 유저의 최근 salt값을 통해 올바른 캡차 값을 입력했는지 검사하는 로직

--- Ticket-Api/src/main/java/com/jnu/ticketapi/api/captcha/service/ValidateCaptchaUseCase.java
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/api/captcha/service/ValidateCaptchaUseCase.java
@@ -1,9 +1,13 @@
 package com.jnu.ticketapi.api.captcha.service;
 
 
+import com.jnu.ticketapi.application.helper.Encryption;
+import com.jnu.ticketapi.config.SecurityUtils;
 import com.jnu.ticketcommon.annotation.UseCase;
 import com.jnu.ticketdomain.domains.captcha.adaptor.CaptchaAdaptor;
+import com.jnu.ticketdomain.domains.captcha.adaptor.CaptchaLogAdaptor;
 import com.jnu.ticketdomain.domains.captcha.domain.Captcha;
+import com.jnu.ticketdomain.domains.captcha.domain.CaptchaLog;
 import com.jnu.ticketdomain.domains.captcha.exception.WrongCaptchaAnswerException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,14 +18,20 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 public class ValidateCaptchaUseCase {
 
+    private final Encryption encryption;
+    private final CaptchaLogAdaptor captchaLogAdaptor;
     private final CaptchaAdaptor captchaAdaptor;
 
     @Transactional
-    public void execute(long captchaId, String answer) {
-        Captcha captcha = captchaAdaptor.findById(captchaId);
+    public void execute(String encryptedCode, String answer) {
+        Long userId = SecurityUtils.getCurrentUserId();
+        CaptchaLog captchaLog = captchaLogAdaptor.findLatestByUserId(userId);
 
-        if (!captcha.validate(answer)) {
+        if (!encryption.validateCaptchaId(encryptedCode, captchaLog.getCaptchaId())) {
             throw WrongCaptchaAnswerException.EXCEPTION;
         }
+
+        Captcha captcha = captchaAdaptor.findById(captchaLog.getCaptchaId());
+        captcha.validate(answer);
     }
 }
