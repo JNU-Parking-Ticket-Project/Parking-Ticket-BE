d459a94ad76c5f71169c45597845df5d8de46575
kssumin <201566@jnu.ac.kr>
2024-10-26 13:23:02 +0900

    chore : lint 적용

--- Ticket-Api/src/test/java/com/jnu/ticketapi/WithCustomMockUser.java
+++ Ticket-Api/src/test/java/com/jnu/ticketapi/WithCustomMockUser.java
@@ -1,16 +1,17 @@
 package com.jnu.ticketapi;
 
-import org.springframework.security.test.context.support.WithSecurityContext;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import org.springframework.security.test.context.support.WithSecurityContext;
 
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @WithSecurityContext(factory = WithCustomUserContextFactory.class)
 public @interface WithCustomMockUser {
     long id() default 1L;
+
     String[] roles() default {"TEST"};
 }
--- Ticket-Api/src/test/java/com/jnu/ticketapi/WithCustomUserContextFactory.java
+++ Ticket-Api/src/test/java/com/jnu/ticketapi/WithCustomUserContextFactory.java
@@ -1,5 +1,8 @@
 package com.jnu.ticketapi;
 
+
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -8,27 +11,23 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithSecurityContextFactory;
 import org.springframework.test.context.ActiveProfiles;
 
-import java.util.Arrays;
-import java.util.List;
-
 @ActiveProfiles("test")
-public class WithCustomUserContextFactory implements WithSecurityContextFactory<WithCustomMockUser> {
+public class WithCustomUserContextFactory
+        implements WithSecurityContextFactory<WithCustomMockUser> {
 
     @Override
     public SecurityContext createSecurityContext(WithCustomMockUser annotation) {
         SecurityContext context = SecurityContextHolder.createEmptyContext();
 
-        List<SimpleGrantedAuthority> authorities = Arrays.stream(annotation.roles())
-                .map(role -> "ROLE_" + role)
-                .map(SimpleGrantedAuthority::new)
-                .toList();
+        List<SimpleGrantedAuthority> authorities =
+                Arrays.stream(annotation.roles())
+                        .map(role -> "ROLE_" + role)
+                        .map(SimpleGrantedAuthority::new)
+                        .toList();
         String username = String.valueOf(annotation.id());
 
-        Authentication authentication = new UsernamePasswordAuthenticationToken(
-                username,
-                "",
-                authorities
-        );
+        Authentication authentication =
+                new UsernamePasswordAuthenticationToken(username, "", authorities);
 
         context.setAuthentication(authentication);
         return context;
