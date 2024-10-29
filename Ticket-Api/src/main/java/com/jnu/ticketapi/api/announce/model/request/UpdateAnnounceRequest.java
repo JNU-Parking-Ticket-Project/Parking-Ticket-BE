e5d080f9f2e0c966f7c538665eb72b596e2447be
Cookie <pon05114@naver.com>
2024-10-29 22:36:49 +0900

    [feat] : 공지사항 수정 ReqDTO 필드 추가

--- Ticket-Api/src/main/java/com/jnu/ticketapi/api/announce/model/request/UpdateAnnounceRequest.java
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/api/announce/model/request/UpdateAnnounceRequest.java
@@ -3,14 +3,30 @@ package com.jnu.ticketapi.api.announce.model.request;
 
 import com.jnu.ticketcommon.annotation.AnnounceContent;
 import com.jnu.ticketcommon.annotation.AnnounceTitle;
+import com.jnu.ticketdomain.domains.announce.domain.Announce;
+import com.jnu.ticketdomain.domains.announce.domain.AnnounceImage;
 import com.jnu.ticketdomain.domains.announce.message.AnnounceValidationMessage;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.Builder;
 
 public record UpdateAnnounceRequest(
         @AnnounceTitle(message = AnnounceValidationMessage.INVALID_TITLE_LENGTH)
                 String announceTitle,
         @AnnounceContent(message = AnnounceValidationMessage.INVALID_CONTENT_LENGTH)
-                String announceContent) {
+                String announceContent,
+        List<String> imageUrl) {
     @Builder
     public UpdateAnnounceRequest {}
+
+    public List<AnnounceImage> from(Announce announce) {
+        return this.imageUrl.stream()
+                .map(
+                        imageUrl ->
+                                AnnounceImage.builder()
+                                        .imageUrl(imageUrl)
+                                        .announce(announce)
+                                        .build())
+                .collect(Collectors.toList());
+    }
 }
