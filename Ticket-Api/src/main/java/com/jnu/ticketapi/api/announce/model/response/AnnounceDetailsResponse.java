41860ce7e7da625fb58fc9857a463109ce349c65
Cookie <pon05114@naver.com>
2024-10-29 22:36:02 +0900

    [feat] : 공지사항 상세조회 ResDTO 필드 추가

--- Ticket-Api/src/main/java/com/jnu/ticketapi/api/announce/model/response/AnnounceDetailsResponse.java
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/api/announce/model/response/AnnounceDetailsResponse.java
@@ -2,23 +2,32 @@ package com.jnu.ticketapi.api.announce.model.response;
 
 
 import com.jnu.ticketdomain.domains.announce.domain.Announce;
+import com.jnu.ticketdomain.domains.announce.domain.AnnounceImage;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.Builder;
 
 public record AnnounceDetailsResponse(
         Long announceId,
         String announceTitle,
         String announceContent,
-        LocalDateTime announceCreatedAt) {
+        LocalDateTime announceCreatedAt,
+        List<String> imageUrls) {
     @Builder
     public AnnounceDetailsResponse {}
 
-    public static AnnounceDetailsResponse from(Announce announce) {
+    public static AnnounceDetailsResponse from(
+            Announce announce, List<AnnounceImage> announceImages) {
         return AnnounceDetailsResponse.builder()
                 .announceId(announce.getAnnounceId())
                 .announceTitle(announce.getAnnounceTitle())
                 .announceContent(announce.getAnnounceContent())
                 .announceCreatedAt(announce.getCreatedAt())
+                .imageUrls(
+                        announceImages.stream()
+                                .map(AnnounceImage::getImageUrl)
+                                .collect(Collectors.toList()))
                 .build();
     }
 }
