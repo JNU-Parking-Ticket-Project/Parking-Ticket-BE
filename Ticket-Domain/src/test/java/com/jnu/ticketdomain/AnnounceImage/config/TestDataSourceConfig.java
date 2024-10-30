a78a97c2900fda854a02f838608ec5dcb6231d28
Cookie <pon05114@naver.com>
2024-10-31 00:56:26 +0900

    [test]: CustomTest Datasource Configuration

---
+++ Ticket-Domain/src/test/java/com/jnu/ticketdomain/AnnounceImage/config/TestDataSourceConfig.java
@@ -0,0 +1,21 @@
+package com.jnu.ticketdomain.AnnounceImage.config;
+
+
+import javax.sql.DataSource;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@TestConfiguration
+public class TestDataSourceConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/ticket");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
+        return dataSource;
+    }
+}
