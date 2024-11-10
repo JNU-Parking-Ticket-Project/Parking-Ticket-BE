c9c054865914b1a2970ad0492e6f6d917010d8a9
Cookie <pon05114@naver.com>
2024-11-10 22:04:31 +0900

    [test]: 학습테스트

---
+++ Ticket-Api/src/test/java/com/jnu/ticketapi/Announce/cache/CacheTestConfiguration.java
@@ -0,0 +1,39 @@
+package com.jnu.ticketapi.Announce.cache;
+
+
+import javax.cache.Caching;
+import javax.cache.configuration.MutableConfiguration;
+import javax.cache.expiry.CreatedExpiryPolicy;
+import javax.cache.expiry.Duration;
+import javax.cache.spi.CachingProvider;
+import org.ehcache.jsr107.EhcacheCachingProvider;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.jcache.JCacheCacheManager;
+import org.springframework.context.annotation.Bean;
+
+@TestConfiguration
+public class CacheTestConfiguration {
+
+    @Bean
+    public CacheService sampleService() {
+        return new CacheService();
+    }
+
+    @Bean
+    public CacheManager ehCacheManager() {
+        CachingProvider cachingProvider =
+                Caching.getCachingProvider(EhcacheCachingProvider.class.getName());
+
+        javax.cache.CacheManager ehCacheManager = cachingProvider.getCacheManager();
+        MutableConfiguration<Object, Object> cacheConfig =
+                new MutableConfiguration<>()
+                        .setStoreByValue(false)
+                        .setExpiryPolicyFactory(
+                                CreatedExpiryPolicy.factoryOf(Duration.TEN_MINUTES));
+
+        ehCacheManager.createCache("myCache", cacheConfig);
+
+        return new JCacheCacheManager(ehCacheManager);
+    }
+}
