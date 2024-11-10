c4b52736b4caaa85aaae9d4b770fccdcf7401ea3
Cookie <pon05114@naver.com>
2024-11-10 22:02:53 +0900

    [feat]: cache log AOP Aspect 추가
    - methodName, result, cacheName 출력
    - log level debug

---
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/common/aop/CacheLoggingAspect.java
@@ -0,0 +1,43 @@
+package com.jnu.ticketapi.common.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.stereotype.Component;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.CachePut;
+
+@Aspect
+@Component
+@Slf4j
+public class CacheLoggingAspect {
+
+    @Around("@annotation(cacheable)")
+    public Object logCacheable(ProceedingJoinPoint joinPoint, Cacheable cacheable) throws Throwable {
+        String methodName = joinPoint.getSignature().toShortString();
+        String cacheName = String.join(", ", cacheable.value());
+        Object result = joinPoint.proceed();
+        log.debug("[CACHE] ANNOTATION(@Cacheable) / METHODNAME({}) / CACHENAME({}) / RESULT({})", methodName, cacheName, result);
+        return result;
+    }
+
+    @Around("@annotation(cachePut)")
+    public Object logCachePut(ProceedingJoinPoint joinPoint, CachePut cachePut) throws Throwable {
+        String methodName = joinPoint.getSignature().toShortString();
+        String cacheName = String.join(", ", cachePut.value());
+        Object result = joinPoint.proceed();
+        log.debug("[CACHE] ANNOTATION(@CachePut) / METHODNAME({}) / CACHENAME({}) / RESULT({})", methodName, cacheName, result);
+        return result;
+    }
+
+    @Around("@annotation(cacheEvict)")
+    public Object logCacheEvict(ProceedingJoinPoint joinPoint, CacheEvict cacheEvict) throws Throwable {
+        String methodName = joinPoint.getSignature().toShortString();
+        String cacheName = String.join(", ", cacheEvict.value());
+        Object result = joinPoint.proceed();
+        log.debug("[CACHE] ANNOTATION(@CacheEvict) / METHODNAME({}) / CACHENAME({}) / RESULT({})", methodName, cacheName, result);
+        return result;
+    }
+}
