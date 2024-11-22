390da93ec5cd1b4faa203604637aac023c0908ef
kssumin <201566@jnu.ac.kr>
2024-11-23 00:59:21 +0900

    fix : 해당 캡차 코드 사용했을 시에 성공 표

--- Ticket-Api/src/main/java/com/jnu/ticketapi/api/captcha/service/ValidateCaptchaUseCase.java
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/api/captcha/service/ValidateCaptchaUseCase.java
@@ -36,5 +36,7 @@ public class ValidateCaptchaUseCase {
         if (!captcha.validate(answer)) {
             throw WrongCaptchaAnswerException.EXCEPTION;
         }
+
+        captchaLog.markSuccess();
     }
 }
