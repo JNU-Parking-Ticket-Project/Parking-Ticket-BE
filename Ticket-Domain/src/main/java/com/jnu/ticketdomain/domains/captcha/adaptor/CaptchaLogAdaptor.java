42f94b19fe8e34a17a24ca4f099ad3e8b330e84e
kssumin <201566@jnu.ac.kr>
2024-11-29 12:40:53 +0900

    refactor : optional 반환

--- Ticket-Domain/src/main/java/com/jnu/ticketdomain/domains/captcha/adaptor/CaptchaLogAdaptor.java
+++ Ticket-Domain/src/main/java/com/jnu/ticketdomain/domains/captcha/adaptor/CaptchaLogAdaptor.java
@@ -19,6 +19,7 @@ public class CaptchaLogAdaptor implements CaptchaLogPort {
 
     @Override
     public CaptchaLog findLatestByUserId(Long userId) {
-        return captchaLogRepository.findTopByUserIdAndIsSuccessFalseOrderByTimestampDesc(userId);
+        return captchaLogRepository.findTopByUserIdAndIsSuccessFalseOrderByTimestampDesc(userId)
+                .orElseThrow(() -> new IllegalArgumentException("dafadsf"));
     }
 }
