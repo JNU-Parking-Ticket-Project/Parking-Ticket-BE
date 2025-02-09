3e8d95ea82fef0fb7bc0598e0f8d6b3bc0579c13
Profile-exe <kang67572346@gmail.com>
2025-02-09 22:22:03 +0900

    feat: 신청 내역에 FE 렌더링을 위한 ID 필드 추가

--- Ticket-Api/src/main/java/com/jnu/ticketapi/api/registration/model/internal/RegistrationDto.java
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/api/registration/model/internal/RegistrationDto.java
@@ -3,11 +3,13 @@ package com.jnu.ticketapi.api.registration.model.internal;
 
 import com.jnu.ticketdomain.domains.registration.domain.Registration;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 import lombok.Builder;
 
 @Builder
 public record RegistrationDto(
+        Long id,
         Long registrationId,
         String name,
         String email,
@@ -18,20 +20,21 @@ public record RegistrationDto(
         String affiliation,
         String sectorNum) {
     public static List<RegistrationDto> of(List<Registration> registrations) {
+        AtomicLong counter = new AtomicLong(1);
         return registrations.stream()
-                .map(
-                        registration ->
-                                RegistrationDto.builder()
-                                        .registrationId(registration.getId())
-                                        .name(registration.getName())
-                                        .email(registration.getEmail())
-                                        .phoneNum(registration.getPhoneNum())
-                                        .studentNum(registration.getStudentNum())
-                                        .isLight(registration.isLight())
-                                        .carNum(registration.getCarNum())
-                                        .affiliation(registration.getAffiliation())
-                                        .sectorNum(registration.getSector().getSectorNumber())
-                                        .build())
+                .map(registration ->
+                        RegistrationDto.builder()
+                                .id(counter.getAndIncrement()) // id 값을 1부터 순차적으로 부여
+                                .registrationId(registration.getId())
+                                .name(registration.getName())
+                                .email(registration.getEmail())
+                                .phoneNum(registration.getPhoneNum())
+                                .studentNum(registration.getStudentNum())
+                                .isLight(registration.isLight())
+                                .carNum(registration.getCarNum())
+                                .affiliation(registration.getAffiliation())
+                                .sectorNum(registration.getSector().getSectorNumber())
+                                .build())
                 .collect(Collectors.toList());
     }
 }
