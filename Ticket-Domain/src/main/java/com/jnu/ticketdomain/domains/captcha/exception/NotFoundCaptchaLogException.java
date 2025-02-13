558473280e9e7fa868cab7f56f1149e0503ce134
Profile-exe <kang67572346@gmail.com>
2025-02-13 11:29:58 +0900

    refactor: 캡챠 로그가 없는 경우에 대한 비즈니스 예외 정의 및 적용

---
+++ Ticket-Domain/src/main/java/com/jnu/ticketdomain/domains/captcha/exception/NotFoundCaptchaLogException.java
@@ -0,0 +1,11 @@
+package com.jnu.ticketdomain.domains.captcha.exception;
+
+import com.jnu.ticketcommon.exception.TicketCodeException;
+
+public class NotFoundCaptchaLogException extends TicketCodeException {
+    public static final TicketCodeException EXCEPTION = new NotFoundCaptchaLogException();
+
+    private NotFoundCaptchaLogException() {
+        super(CaptchaErrorCode.NOT_FOUND_CAPTCHA_LOG);
+    }
+}
