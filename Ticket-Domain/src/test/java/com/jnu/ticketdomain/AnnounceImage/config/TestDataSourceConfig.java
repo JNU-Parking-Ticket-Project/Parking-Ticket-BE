cb0f56ccbdcd984cadf35a3f9897ba3a4ca389ba
Jin Hyuk <ekrrdj07@gmail.com>
2025-02-14 22:37:07 +0900

    test: 테스트용 mysql에 dialect 주입

--- Ticket-Domain/src/test/java/com/jnu/ticketdomain/AnnounceImage/config/TestDataSourceConfig.java
+++ Ticket-Domain/src/test/java/com/jnu/ticketdomain/AnnounceImage/config/TestDataSourceConfig.java
@@ -1,10 +1,14 @@
 package com.jnu.ticketdomain.AnnounceImage.config;
 
 
-import javax.sql.DataSource;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+import java.util.Properties;
 
 @TestConfiguration
 public class TestDataSourceConfig {
@@ -17,5 +21,23 @@ public class TestDataSourceConfig {
         dataSource.setUsername("root");
         dataSource.setPassword("1234");
         return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+        emf.setDataSource(dataSource);
+        // 패키지 스캔 경로를 실제 엔티티들이 위치한 패키지로 지정
+        emf.setPackagesToScan("com.jnu.ticketdomain.domains");
+
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        emf.setJpaVendorAdapter(vendorAdapter);
+
+        Properties jpaProperties = new Properties();
+        jpaProperties.put("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+        // 필요한 추가 JPA 속성들을 설정
+        emf.setJpaProperties(jpaProperties);
+
+        return emf;
     }
 }
