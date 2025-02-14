ec4e05314f54aa3fc0273fc7fd3725b553f11d1a
Cookie <pon05114@naver.com>
2025-02-14 22:53:46 +0900

    bug: fixed test bug

--- Ticket-Domain/src/test/java/com/jnu/ticketdomain/AnnounceImage/config/TestDataSourceConfig.java
+++ Ticket-Domain/src/test/java/com/jnu/ticketdomain/AnnounceImage/config/TestDataSourceConfig.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 import java.util.Properties;
 
@@ -17,29 +15,19 @@ public class TestDataSourceConfig {
     @Bean
     @Primary
     public DataSource dataSource() {
+        Properties properties = new Properties();
+        properties.setProperty("useSSL", "false");
+        properties.setProperty("serverTimezone", "Asia/Seoul");
+        properties.setProperty("characterEncoding", "UTF-8");
+        properties.setProperty("allowPublicKeyRetrieval", "true");
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://localhost:3306/ticket");
         dataSource.setUsername("root");
-        dataSource.setPassword("1234");
+        dataSource.setPassword("");
+        dataSource.setCatalog("ticket");
+        dataSource.setConnectionProperties(properties);
         return dataSource;
     }
 
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
-        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
-        emf.setDataSource(dataSource);
-        // 패키지 스캔 경로를 실제 엔티티들이 위치한 패키지로 지정
-        emf.setPackagesToScan("com.jnu.ticketdomain.domains");
-
-        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        emf.setJpaVendorAdapter(vendorAdapter);
-
-        Properties jpaProperties = new Properties();
-        jpaProperties.put("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
-        // 필요한 추가 JPA 속성들을 설정
-        emf.setJpaProperties(jpaProperties);
-
-        return emf;
-    }
 }
