b20727134ebaf34963b6b09da219b63949ff5560
Profile-exe <kang67572346@gmail.com>
2025-02-14 18:30:38 +0900

    refactor: 코드 검증 이후 반환된 Captcha ID를 사용하도록 변경 및 불필요한 의존성 제거

--- Ticket-Api/src/main/java/com/jnu/ticketapi/api/captcha/service/ValidateCaptchaUseCase.java
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/api/captcha/service/ValidateCaptchaUseCase.java
@@ -4,9 +4,7 @@ package com.jnu.ticketapi.api.captcha.service;
 import com.jnu.ticketapi.config.SecurityUtils;
 import com.jnu.ticketcommon.annotation.UseCase;
 import com.jnu.ticketdomain.domains.captcha.adaptor.CaptchaAdaptor;
-import com.jnu.ticketdomain.domains.captcha.adaptor.CaptchaLogAdaptor;
 import com.jnu.ticketdomain.domains.captcha.domain.Captcha;
-import com.jnu.ticketdomain.domains.captcha.domain.CaptchaLog;
 import com.jnu.ticketdomain.domains.captcha.exception.WrongCaptchaAnswerException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,16 +16,14 @@ import org.springframework.transaction.annotation.Transactional;
 public class ValidateCaptchaUseCase {
 
     private final CaptchaAdaptor captchaAdaptor;
-    private final CaptchaLogAdaptor captchaLogAdaptor;
     private final CaptchaHashProcessor captchaHashProcessor;
 
     @Transactional
     public void execute(String encryptedCode, String answer) {
         Long userId = SecurityUtils.getCurrentUserId();
-        captchaHashProcessor.verify(encryptedCode, userId);
+        Long captchaId = captchaHashProcessor.verify(encryptedCode, userId);
 
-        CaptchaLog captchaLog = captchaLogAdaptor.findLatestByUserId(userId);
-        Captcha captcha = captchaAdaptor.findById(captchaLog.getCaptchaId());
+        Captcha captcha = captchaAdaptor.findById(captchaId);
         if (!captcha.validate(answer)) {
             throw WrongCaptchaAnswerException.EXCEPTION;
         }
