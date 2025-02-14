0d6f4d46ea86cffafedaae80776bb8931fcf250d
Cookie <pon05114@naver.com>
2025-02-14 21:55:33 +0900

    bug: fixed test bug

--- Ticket-Domain/src/test/java/com/jnu/ticketdomain/AnnounceImage/config/TestDataSourceConfig.java
+++ Ticket-Domain/src/test/java/com/jnu/ticketdomain/AnnounceImage/config/TestDataSourceConfig.java
@@ -4,12 +4,14 @@ package com.jnu.ticketdomain.AnnounceImage.config;
 import javax.sql.DataSource;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @TestConfiguration
 public class TestDataSourceConfig {
 
     @Bean
+    @Primary
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
