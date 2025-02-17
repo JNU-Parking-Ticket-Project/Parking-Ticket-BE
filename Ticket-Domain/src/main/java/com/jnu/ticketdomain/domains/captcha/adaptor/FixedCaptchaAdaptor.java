6cadc6e5bd0f9c011d6336d52774b959b7eb7846
Profile-exe <kang67572346@gmail.com>
2025-02-18 01:09:04 +0900

    feat: fixed 환경 CaptchaAdaptor 구현 및 random 환경 CaptchaAdaptor 식별자 변경
    - 각 환경에 따라 ConditionalOnProperty 적용

--- Ticket-Domain/src/main/java/com/jnu/ticketdomain/domains/captcha/adaptor/FixedCaptchaAdaptor.java
+++ Ticket-Domain/src/main/java/com/jnu/ticketdomain/domains/captcha/adaptor/FixedCaptchaAdaptor.java
@@ -6,21 +6,20 @@ import com.jnu.ticketdomain.domains.captcha.domain.Captcha;
 import com.jnu.ticketdomain.domains.captcha.exception.NotFoundCaptchaException;
 import com.jnu.ticketdomain.domains.captcha.out.CaptchaLoadPort;
 import com.jnu.ticketdomain.domains.captcha.repository.CaptchaRepository;
-import java.util.concurrent.ThreadLocalRandom;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 
 @RequiredArgsConstructor
+@ConditionalOnProperty(prefix = "encryption", name = "salt-type", havingValue = "fixed")
 @Adaptor
-public class CaptchaAdaptor implements CaptchaLoadPort {
+public class FixedCaptchaAdaptor implements CaptchaLoadPort {
     private final CaptchaRepository captchaRepository;
 
     @Override
-    public Captcha findByRandom() {
-        long captchaTotalCount = captchaRepository.count();
-        long randomOffset = ThreadLocalRandom.current().nextLong(captchaTotalCount);
-
-        // id가 offset + 1 인 캡챠 조회
-        return captchaRepository.findOneByOffset(randomOffset);
+    public Captcha findByRandom() { // fixed
+        return captchaRepository
+                .findFirstByOrderByIdAsc()
+                .orElseThrow(() -> NotFoundCaptchaException.EXCEPTION);
     }
 
     @Override
---
+++ Ticket-Domain/src/main/java/com/jnu/ticketdomain/domains/captcha/adaptor/RandomCaptchaAdaptor.java
@@ -0,0 +1,32 @@
+package com.jnu.ticketdomain.domains.captcha.adaptor;
+
+
+import com.jnu.ticketcommon.annotation.Adaptor;
+import com.jnu.ticketdomain.domains.captcha.domain.Captcha;
+import com.jnu.ticketdomain.domains.captcha.exception.NotFoundCaptchaException;
+import com.jnu.ticketdomain.domains.captcha.out.CaptchaLoadPort;
+import com.jnu.ticketdomain.domains.captcha.repository.CaptchaRepository;
+import java.util.concurrent.ThreadLocalRandom;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+
+@RequiredArgsConstructor
+@ConditionalOnProperty(prefix = "encryption", name = "salt-type", havingValue = "random")
+@Adaptor
+public class RandomCaptchaAdaptor implements CaptchaLoadPort {
+    private final CaptchaRepository captchaRepository;
+
+    @Override
+    public Captcha findByRandom() {
+        long captchaTotalCount = captchaRepository.count();
+        long randomOffset = ThreadLocalRandom.current().nextLong(captchaTotalCount);
+
+        // id가 offset + 1 인 캡챠 조회
+        return captchaRepository.findOneByOffset(randomOffset);
+    }
+
+    @Override
+    public Captcha findById(long id) {
+        return captchaRepository.findById(id).orElseThrow(() -> NotFoundCaptchaException.EXCEPTION);
+    }
+}
