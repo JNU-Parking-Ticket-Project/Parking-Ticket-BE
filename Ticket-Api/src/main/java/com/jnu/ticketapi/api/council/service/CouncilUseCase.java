b538dcd259325e2b2b8b73bc2d110fa1aba7aaf2
xxbeann <yjb4945@gmail.com>
2025-02-18 04:27:05 +0900

    feat: 이메일 전송 API 구현

--- Ticket-Api/src/main/java/com/jnu/ticketapi/api/council/service/CouncilUseCase.java
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/api/council/service/CouncilUseCase.java
@@ -1,6 +1,7 @@
 package com.jnu.ticketapi.api.council.service;
 
 
+import com.jnu.ticketapi.api.council.handler.EmailSendEventHandler;
 import com.jnu.ticketapi.api.council.model.request.SignUpCouncilRequest;
 import com.jnu.ticketapi.api.council.model.response.SignUpCouncilResponse;
 import com.jnu.ticketcommon.annotation.UseCase;
@@ -8,17 +9,25 @@ import com.jnu.ticketcommon.message.ResponseMessage;
 import com.jnu.ticketdomain.domains.council.adaptor.CouncilAdaptor;
 import com.jnu.ticketdomain.domains.council.domain.Council;
 import com.jnu.ticketdomain.domains.council.exception.AlreadyExistEmailException;
+import com.jnu.ticketdomain.domains.events.event.SendEmailEvent;
+import com.jnu.ticketdomain.domains.registration.adaptor.RegistrationAdaptor;
 import com.jnu.ticketdomain.domains.user.adaptor.UserAdaptor;
 import com.jnu.ticketdomain.domains.user.domain.User;
 import com.jnu.ticketdomain.domains.user.exception.NotFoundUserException;
+import com.jnu.ticketinfrastructure.service.MailService;
+import lombok.extern.slf4j.Slf4j;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @UseCase
 @RequiredArgsConstructor
 public class CouncilUseCase {
     private final CouncilAdaptor councilAdaptor;
     private final UserAdaptor userAdaptor;
+    private final ApplicationEventPublisher eventPublisher;
 
     @Transactional(readOnly = true)
     public User findByEmail(String email) {
@@ -36,5 +45,15 @@ public class CouncilUseCase {
         Council council = signUpCouncilRequest.toCouncilEntity(signUpCouncilRequest, jpaUser);
         councilAdaptor.save(council);
         return SignUpCouncilResponse.of(ResponseMessage.SUCCESS_SIGN_UP);
+    }
+
+    @Transactional
+    public void sendEmail(Long eventId) {
+        try {
+            eventPublisher.publishEvent(new SendEmailEvent(eventId)); // 이벤트 객체 발행
+            log.info("SendEmailEvent published for eventId: {}", eventId);
+        } catch (Exception e) {
+            log.error("Failed to publish SendEmailEvent: {}", e.getMessage());
+        }
     }
 }
