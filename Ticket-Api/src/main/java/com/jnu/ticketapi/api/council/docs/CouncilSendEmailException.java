d9f95c5cf1008925a7ca54de7e55f8c462b0e0db
Jin Hyuk <ekrrdj07@gmail.com>
2025-02-18 17:06:43 +0900

    feat: 예외 컨벤션 적용

--- Ticket-Api/src/main/java/com/jnu/ticketapi/api/council/docs/CouncilSendEmailException.java
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/api/council/docs/CouncilSendEmailException.java
@@ -9,5 +9,5 @@ import com.jnu.ticketdomain.domains.council.exception.SendFailEmailException;
 @ExceptionDoc
 public class CouncilSendEmailException implements SwaggerExampleExceptions {
     @ExplainError("이메일 전송 요청이 실패한 경우")
-    public TicketCodeException 이메일_전송이_실패하였습니다 = SendFailEmailException.FAIL_TO_SEND_EMAIL;
+    public TicketCodeException 이메일_전송이_실패하였습니다 = SendFailEmailException.EXCEPTION;
 }
--- Ticket-Domain/src/main/java/com/jnu/ticketdomain/domains/council/exception/SendFailEmailException.java
+++ Ticket-Domain/src/main/java/com/jnu/ticketdomain/domains/council/exception/SendFailEmailException.java
@@ -3,7 +3,7 @@ package com.jnu.ticketdomain.domains.council.exception;
 import com.jnu.ticketcommon.exception.TicketCodeException;
 
 public class SendFailEmailException extends TicketCodeException {
-    public static final TicketCodeException FAIL_TO_SEND_EMAIL = new SendFailEmailException();
+    public static final TicketCodeException EXCEPTION = new SendFailEmailException();
 
     private SendFailEmailException() {
         super(CouncilErrorCode.FAILED_TO_SEND_EMAIL);
