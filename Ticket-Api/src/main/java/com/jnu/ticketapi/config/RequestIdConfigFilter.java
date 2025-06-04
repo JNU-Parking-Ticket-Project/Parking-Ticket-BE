03d46ba5d1713efc5de69707545f4c5dbacbd2b5
aiaiaiai1 <cvc100326@naver.com>
2025-06-04 11:45:24 +0900

    refactor: 식별자 생성 방식 보완
    - 8자리임을 보장

--- Ticket-Api/src/main/java/com/jnu/ticketapi/config/RequestIdConfigFilter.java
+++ Ticket-Api/src/main/java/com/jnu/ticketapi/config/RequestIdConfigFilter.java
@@ -16,6 +16,8 @@ public class RequestIdConfigFilter extends OncePerRequestFilter {
 
     private static final String REQUEST_ID_KEY = "requestId";
 
+    private static final String CHAR_POOL = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         MDC.put(REQUEST_ID_KEY, generateRequestId());
@@ -26,14 +28,9 @@ public class RequestIdConfigFilter extends OncePerRequestFilter {
         }
     }
 
-    private String generateRequestId() {
+    public String generateRequestId() {
         return IntStream.range(0, 8)
-                .mapToObj(i -> String.valueOf((char) ThreadLocalRandom.current().nextInt(48, 123)))
-                .filter(
-                        ch ->
-                                (ch.charAt(0) >= '0' && ch.charAt(0) <= '9')
-                                        || (ch.charAt(0) >= 'A' && ch.charAt(0) <= 'Z')
-                                        || (ch.charAt(0) >= 'a' && ch.charAt(0) <= 'z'))
+                .mapToObj(i -> String.valueOf(CHAR_POOL.charAt(ThreadLocalRandom.current().nextInt(CHAR_POOL.length()))))
                 .collect(Collectors.joining());
     }
 
