69a2ef94cc6d724f30323dc7695d65d5d5432c19
Jin Hyuk <ekrrdj07@gmail.com>
2025-06-24 17:37:29 +0900

    refactor: 신청목록 정렬을 QueryDSL로 적용

--- Ticket-Domain/src/main/java/com/jnu/ticketdomain/domains/registration/repository/RegistrationRepositoryCustom.java
+++ Ticket-Domain/src/main/java/com/jnu/ticketdomain/domains/registration/repository/RegistrationRepositoryCustom.java
@@ -1,11 +1,16 @@
 package com.jnu.ticketdomain.domains.registration.repository;
 
 
+import com.jnu.ticketdomain.domains.registration.domain.Registration;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RegistrationRepositoryCustom {
     Boolean existsByEmailAndIsSavedTrueAndEvent(String email, Long eventId);
 
     Boolean existsByStudentNumAndIsSavedTrue(String studentNum, Long eventId);
+
+    List<Registration> findSortedRegistrationsByEventId(Long eventId);
 }
--- Ticket-Domain/src/main/java/com/jnu/ticketdomain/domains/registration/repository/RegistrationRepositoryCustomImpl.java
+++ Ticket-Domain/src/main/java/com/jnu/ticketdomain/domains/registration/repository/RegistrationRepositoryCustomImpl.java
@@ -3,11 +3,16 @@ package com.jnu.ticketdomain.domains.registration.repository;
 import static com.jnu.ticketdomain.domains.registration.domain.QRegistration.registration;
 
 import com.jnu.ticketdomain.domains.registration.domain.QRegistration;
+import com.jnu.ticketdomain.domains.registration.domain.Registration;
+import com.jnu.ticketdomain.domains.user.domain.UserStatus;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import javax.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -15,16 +20,15 @@ public class RegistrationRepositoryCustomImpl implements RegistrationRepositoryC
     private final EntityManager em;
     private final JPAQueryFactory queryFactory;
 
+    QRegistration r = QRegistration.registration;
+
     @Override
     public Boolean existsByEmailAndIsSavedTrueAndEvent(String email, Long eventId) {
         return queryFactory
                         .selectOne()
-                        .from(registration)
+                        .from(r)
                         .where(
-                                registration
-                                        .email
-                                        .eq(email)
-                                        .and(isSavedAndEqEvent(registration, eventId)))
+                                r.email.eq(email).and(isSavedAndEqEvent(registration, eventId)))
                         .fetchFirst()
                 != null;
     }
@@ -33,15 +37,43 @@ public class RegistrationRepositoryCustomImpl implements RegistrationRepositoryC
     public Boolean existsByStudentNumAndIsSavedTrue(String studentNum, Long eventId) {
         return queryFactory
                         .selectOne()
-                        .from(registration)
+                        .from(r)
                         .where(
-                                registration
-                                        .studentNum
-                                        .eq(studentNum)
-                                        .and(isSavedAndEqEvent(registration, eventId)))
+                                r.studentNum.eq(studentNum).and(isSavedAndEqEvent(registration, eventId)))
                         .fetchFirst()
                 != null;
     }
+
+    @Override
+    public List<Registration> findSortedRegistrationsByEventId(Long eventId) {
+
+        var u = r.user;
+        var s = r.sector;
+
+        return queryFactory
+                .selectFrom(r)
+                .where(
+                        r.isDeleted.isFalse(),
+                        r.isSaved.isTrue(),
+                        s.event.id.eq(eventId),
+                        u.status.in(UserStatus.SUCCESS, UserStatus.PREPARE)
+                )
+                .orderBy(
+                        s.sectorNumber.asc(),
+                        // SUCCESS 먼저 오도록 정렬
+                        new CaseBuilder()
+                                .when(u.status.eq(UserStatus.SUCCESS)).then(0)
+                                .otherwise(1)
+                                .asc(),
+                        // SUCCESS면 r.id, PREPARE면 u.sequence
+                        new CaseBuilder()
+                                .when(u.status.eq(UserStatus.SUCCESS)).then(r.id)
+                                .otherwise(u.sequence.longValue())
+                                .asc()
+                )
+                .fetch();
+    }
+
 
     private BooleanExpression isSavedAndEqEvent(QRegistration registration, Long eventId) {
         return registration.isSaved.isTrue().and(registration.sector.event.id.eq(eventId));
