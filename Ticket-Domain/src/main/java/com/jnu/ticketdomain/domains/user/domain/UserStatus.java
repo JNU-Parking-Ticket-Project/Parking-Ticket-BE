d18029ccdf20856145fbe7a64ac350a9a39f71a6
Jin Hyuk <ekrrdj07@gmail.com>
2025-06-24 17:40:49 +0900

    refactor: 불필요한 주석 제거

--- Ticket-Domain/src/main/java/com/jnu/ticketdomain/domains/user/domain/UserStatus.java
+++ Ticket-Domain/src/main/java/com/jnu/ticketdomain/domains/user/domain/UserStatus.java
@@ -7,7 +7,6 @@ import lombok.Getter;
 @EnumClass
 @Getter
 public enum UserStatus {
-    // 절대 이 순서를 바꾸지 마세요. 신청서를 조회할 때 합격자가 예비자보다 먼저 나오게 하기 위함입니다.
     SUCCESS("합격"),
     PREPARE("예비"),
     FAIL("불합격");
